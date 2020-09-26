a1fc9f5a5b8c9f949a5b868665d8ffdf11d28823
Bryon Tjanaka <38124174+btjanaka@users.noreply.github.com>
2020-09-26 03:02:40 -0700

    Improve docs rendering (#23)
    * Make docs serving track library files as well
    * Add Napoleon for displaying Google-style docstrings
    * Add mathjax to render math

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,7 @@
 #
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('..'))
 
 import ribs
@@ -31,7 +32,18 @@ import ribs
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.mathjax',
+]
+
+# Napoleon
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+napoleon_use_param = False
+napoleon_use_ivar = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
