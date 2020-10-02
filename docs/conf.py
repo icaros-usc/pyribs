98f457aeea251e326df5723e481f0072d6cc80c7
Bryon Tjanaka <38124174+btjanaka@users.noreply.github.com>
2020-10-01 18:22:37 -0700

    Switch to Sphinx Book Theme for docs (#24)
    * Remove RTD theme
    * Add temporary logos
    * Add additional config options for book theme

--- docs/conf.py
+++ docs/conf.py
@@ -94,13 +94,21 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
+html_logo = "_static/logo.png"
+html_favicon = "_static/favicon.ico"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    "repository_url": "https://github.com/icaros-usc/pyribs/",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_edit_page_button": True,
+    "path_to_docs": "docs/",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
