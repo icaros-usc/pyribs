c4b37224d070b614456d9654d9443909cf2f7bb0
btjanaka <bryon@btjanaka.net>
2020-10-23 12:51:44 -0700

    Reconstruct archive on each benchmark itr

--- tests/archives/grid_archive_benchmark.py
+++ tests/archives/grid_archive_benchmark.py
@@ -7,15 +7,15 @@ from ribs.archives import GridArchive
 
 
 def benchmark_add_hundred_thousand_entries(benchmark):
-    archive = GridArchive([64, 64], [(-1, 1), (-1, 1)])
-
     itrs = int(1e5)
 
     solutions = np.random.uniform(-1, 1, (itrs, 2))
-    behavior_values = solutions
     objective_values = np.random.uniform(-10, 10, itrs)
+    behavior_values = solutions
 
     @benchmark
     def insert_hundred_thousand_entries():
+        archive = GridArchive([64, 64], [(-1, 1), (-1, 1)])
+        assert archive.is_empty()
         for i in range(itrs):
             archive.add(solutions[i], objective_values[i], behavior_values[i])
