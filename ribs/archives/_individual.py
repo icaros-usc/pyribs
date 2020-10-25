0225c31630db4a163e880fff9ae8f261f666639a
btjanaka <bryon@btjanaka.net>
2020-10-25 01:16:22 -0700

    Re-implement individual as namedtuple for 3.6 compat

--- ribs/archives/_individual.py
+++ ribs/archives/_individual.py
@@ -1,34 +1,17 @@
 """Provides an Individual class that represents archive entries."""
 
-from dataclasses import dataclass
+from collections import namedtuple
 
-import numpy as np
-
-
-def _ndarray_eq(a1, a2):
-    """Performs a stricter check of whether two numpy arrays are equal."""
-    return (isinstance(a1, np.ndarray) and isinstance(a2, np.ndarray) and
-            a1.shape == a2.shape and np.all(a1 == a2))
-
-
-@dataclass(eq=False)
-class Individual:
-    """Holds data of a single archive entry.
-
-    Attributes:
-        objective_value (float): The evaluated performance of this individual's
-            solution.
-        behavior_values (np.ndarray): An array with the individual's coordinates
-            in behavior space.
-        solution (np.ndarray): An array with the individual's solution.
-    """
-    objective_value: float
-    behavior_values: np.ndarray
-    solution: np.ndarray
-
-    def __eq__(self, other):
-        if not isinstance(other, Individual):
-            return NotImplemented
-        return (self.objective_value == other.objective_value and
-                _ndarray_eq(self.behavior_values, other.behavior_values) and
-                _ndarray_eq(self.solution, other.solution))
+#: Holds data of a single archive entry.
+#:
+#: Attributes:
+#:     objective_value (float): The evaluated performance of this individual's
+#:         solution.
+#:     behavior_values (np.ndarray): An array with the individual's coordinates
+#:         in behavior space.
+#:     solution (np.ndarray): An array with the individual's solution.
+Individual = namedtuple("Individual", [
+    "objective_value",
+    "behavior_values",
+    "solution",
+])
