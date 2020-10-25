cc6c60c6f538cf0d72a12699ea9dce81abbcb569
btjanaka <bryon@btjanaka.net>
2020-10-25 04:22:35 -0700

    Add config functions

--- ribs/config.py
+++ ribs/config.py
@@ -3,32 +3,95 @@
 __all__ = [
     "DEFAULT_CONFIG",
     "update",
+    "merge_with_default_config",
 ]
 
 # TODO: Print out this config on some page.
 
 #: Default configuration.
 DEFAULT_CONFIG = {
-    # This is a comment.
-    "option1": 2.0,
+    # (int) The size of each batch.
+    "batch_size": 64,
 
-    # This is another comment.
-    "option2": 3.0,
+    # Seed for random number generators. Leave None to avoid seeding.
+    "seed": None,
 }
 
 
-def update(new, default=DEFAULT_CONFIG):
-    """Merges the two configuration dicts, overriding ``default`` with ``new``
+def update(default, new):
+    """Merges two configuration dicts, overriding ``default`` with ``new``.
+
+    ``default`` is modified in-place with the new options.
+
+    Handles nested dict options as well.
+
+    Note that ``new`` is allowed to provide new keys to ``default``.
+
+    If we have:
+
+        ::
+
+            default = {
+                "a": 1,
+                "b": {
+                    "c": 2,
+                    "d": 3,
+                },
+            }
+
+            new = {
+                "a": 4,
+                "b": {
+                    "c": 5,
+                },
+                "e": 6,
+            }
+
+    Then ``update(default, new)`` modifies ``default`` to be:
+
+        ::
+
+            default = {
+                "a": 4,
+                "b": {
+                    "c": 5,
+                    "d": 3,
+                }
+                "e": 6,
+            }
 
     Args:
+        default (dict): Default configuration options.
         new (dict): New configuration options.
-        default (dict): Default configuration options. Passing in this parameter
-            is not recommended unless you have your own default config.
-    Returns:
-        A dict where the options from ``new`` have overridden those in
-        ``default``.
+    Raises:
+        TypeError: When one attempts to override a non-dict value with a dict
+        value.
     """
+    for key in new:
+        if isinstance(new[key], dict):
+            if key not in default:
+                default[key] = {}
+            if not isinstance(default[key], dict):
+                raise TypeError(f"The value at key `{key}` in new is a dict, "
+                                f"but in default, it is {default[key]}")
+            update(default[key], new[key])
+        else:
+            default[key] = new[key]
 
-    # TODO: Examples in docstring
 
-    return default
+def merge_with_default_config(config):
+    """Creates a configuration that updates ``DEFAULT_CONFIG`` with ``config``.
+
+    Values in ``DEFAULT_CONFIG`` are overridden by those in ``config``,
+    including those in nested dicts.
+
+    Args:
+        config (dict): A configuration dict to use to update ``DEFAULT_CONFIG``.
+    Returns:
+        dict: An updated version of ``DEFAULT_CONFIG``. The update is made with
+        ``update``.
+    """
+    new_config = {}
+    update(new_config, DEFAULT_CONFIG)
+    update(new_config, config)
+    return new_config
---
+++ tests/config_test.py
@@ -0,0 +1,134 @@
+"""Tests for ribs config functions."""
+import pytest
+
+from ribs.config import DEFAULT_CONFIG, merge_with_default_config, update
+
+# pylint: disable = invalid-name
+
+
+def test_update_overrides_single_dict():
+    default = {
+        "a": 1,
+        "b": 2,
+        "c": 3,
+    }
+    new = {
+        "b": 4,
+    }
+    update(default, new)
+
+    assert default == {
+        "a": 1,
+        "b": 4,
+        "c": 3,
+    }
+
+
+def test_update_overrides_nested_dict():
+    default = {
+        "a": 1,
+        "b": {
+            "c": 2,
+            "d": 3,
+        },
+    }
+    new = {
+        "a": 4,
+        "b": {
+            "c": 5,
+        },
+    }
+    update(default, new)
+
+    assert default == {
+        "a": 4,
+        "b": {
+            "c": 5,
+            "d": 3,
+        }
+    }
+
+
+def test_update_adds_new_keys():
+    default = {
+        "a": 1,
+        "b": {
+            "c": 2,
+            "d": 3,
+        },
+    }
+
+    new = {
+        "a": 4,
+        "b": {
+            "c": 5,
+            "e": 6,
+        },
+        "f": 7,
+    }
+    update(default, new)
+
+    assert default == {
+        "a": 4,
+        "b": {
+            "c": 5,
+            "d": 3,
+            "e": 6,
+        },
+        "f": 7,
+    }
+
+
+def test_update_deeper_nested_dict():
+    default = {
+        "a": {
+            "b": {
+                "c": {
+                    "d": {
+                        "e": 1,
+                    },
+                },
+            },
+        },
+    }
+    new = {
+        "a": {
+            "b": {
+                "c": {
+                    "d": {
+                        "e": 6,
+                    },
+                },
+            },
+        },
+    }
+    update(default, new)
+
+    assert default == {
+        "a": {
+            "b": {
+                "c": {
+                    "d": {
+                        "e": 6,
+                    },
+                },
+            },
+        },
+    }
+
+
+def test_override_non_dict_val_fails():
+    with pytest.raises(TypeError):
+        default = {
+            "a": 1,
+        }
+        new = {
+            "a": {
+                "b": 2,
+            },
+        }
+        update(default, new)
+
+
+def test_merging_default_with_nothing_gives_default():
+    assert merge_with_default_config({}) == DEFAULT_CONFIG
