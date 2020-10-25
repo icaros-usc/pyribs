c52e3d249c9b4f2941919ee342ef2019c35befd8
btjanaka <bryon@btjanaka.net>
2020-10-25 02:48:29 -0700

    Tweak benchmarks

--- tests/archives/grid_archive_benchmark.py
+++ tests/archives/grid_archive_benchmark.py
@@ -7,25 +7,17 @@ from ribs.archives import GridArchive
 # See conftest.py for benchmark_data_100k.
 
 
-def benchmark_construct_and_add_hundred_thousand_entries(
-        benchmark, benchmark_data_100k):
+def benchmark_construct_and_add_100k(benchmark, benchmark_data_100k):
     n, solutions, objective_values, behavior_values = benchmark_data_100k
 
-    def insert_hundred_thousand_entries(archive):
+    @benchmark
+    def insert_hundred_thousand_entries():
+        archive = GridArchive((64, 64), [(-1, 1), (-1, 1)])
         for i in range(n):
             archive.add(solutions[i], objective_values[i], behavior_values[i])
 
-    def setup():
-        return (GridArchive((64, 64), [(-1, 1), (-1, 1)]),), {}
 
-    benchmark.pedantic(insert_hundred_thousand_entries,
-                       setup=setup,
-                       rounds=5,
-                       iterations=1)
-
-
-def benchmark_get_random_elite_hundred_thousand_times(benchmark,
-                                                      benchmark_data_100k):
+def benchmark_get_100k_random_elites(benchmark, benchmark_data_100k):
     n, solutions, objective_values, behavior_values = benchmark_data_100k
     archive = GridArchive((64, 64), [(-1, 1), (-1, 1)])
     for i in range(n):
