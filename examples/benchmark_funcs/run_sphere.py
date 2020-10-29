3636063976058688e5c69d17651326314b8b8fd1
btjanaka <bryon@btjanaka.net>
2020-10-29 12:36:51 -0700

    Make run_sphere use sphere func

--- examples/benchmark_funcs/run_sphere.py
+++ examples/benchmark_funcs/run_sphere.py
@@ -1,8 +1,19 @@
+"""Runs MAP-Elites on the Sphere function."""
 import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
 
 from ribs.archives import GridArchive
 from ribs.optimizers import Optimizer
+
+
+def sphere(sol):
+    """The sphere function is the sum of squared components of the solution.
+
+    We return the negative because MAP-Elites seeks to maximize.
+    """
+    return -np.sum(np.square(sol))
+
 
 if __name__ == '__main__':
 
@@ -15,18 +26,22 @@ if __name__ == '__main__':
 
     for i in range(10**5):
         sols = opt.ask()
-        objs = [sum(s) for s in sols]
-        bcs = [(s[0], s[1]) for s in sols]
+        objs = [sphere(s) for s in sols]
+        bcs = [(np.sum(s[:5]), np.sum(s[5:])) for s in sols]
 
         opt.tell(sols, objs, bcs)
 
         if i % 1000 == 0:
             print('saving {}'.format(i))
-            #data = opt.archive.as_pandas()
-            #data = data.pivot('index-0', 'index-1', 'objective')
+            #  data = opt.archive.as_pandas()
+            #  data = data.pivot('index-0', 'index-1', 'objective')
 
-            #ax = sns.heatmap(data)
-            #plt.savefig('images/arc-{:05d}'.format(i))
-            #plt.close()
+            #  ax = sns.heatmap(data)
+            #  plt.savefig('images/arc-{:05d}'.format(i))
+            #  plt.close()
 
-    print(archive.as_pandas().head())
+    data = archive.as_pandas()
+    print(data.head())
+    data = data.pivot('index-0', 'index-1', 'objective')
+    sns.heatmap(data)
+    plt.savefig('sphere-map-elites.png')
