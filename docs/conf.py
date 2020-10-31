98e37a2f80daa2eea57fcbb72ba03955ed56d22d
btjanaka <bryon@btjanaka.net>
2020-10-31 12:27:08 -0700

    Custom docs title

--- docs/conf.py
+++ docs/conf.py
@@ -100,6 +100,7 @@ todo_include_todos = False
 html_theme = 'sphinx_book_theme'
 html_logo = "_static/logo.png"
 html_favicon = "_static/favicon.ico"
+html_title = "pyribs v" + ribs.__version__
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
