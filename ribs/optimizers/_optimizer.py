6687f2d3b27849cf8364f04fa61dd7341fe2f59f
btjanaka <bryon@btjanaka.net>
2020-11-06 20:59:34 -0800

    Add docstrings for optimizers

--- ribs/optimizers/_optimizer.py
+++ ribs/optimizers/_optimizer.py
@@ -16,42 +16,87 @@ class OptimizerConfig:
 
 
 class Optimizer:
+    """A basic class that composes an archive with multiple emitters.
+
+    To use this class, first create an archive and list of emitters for your
+    QD algorithm. Then, construct the Optimizer with these arguments. Finally,
+    repeatedly call :meth:`ask` method to collect solutions to analyze, and
+    return the objective values and behavior values of those solutions **in the
+    same order** using :meth:`tell`.
+
+    Args:
+        archive (ribs archive): An archive object, selected from
+            :mod:`ribs.archives`.
+        emitters (list of ribs emitters): A list of emitter objects, each
+            selected from :mod:`ribs.emitters`.
+        config (None or dict or OptimizerConfig): Configuration object. If None,
+            a default OptimizerConfig is constructed. A dict may also be passed
+            in, in which case its arguments will be passed into OptimizerConfig.
+    Attributes:
+        config (OptimizerConfig): Configuration object.
+        archive (ribs archive): See args.
+        emitters (list of ribs emitters): See args.
+    """
 
     def __init__(self, archive, emitters, config=None):
         self.config = create_config(config, OptimizerConfig)
-
         self.archive = archive
-        self.num_iters = 0
-        self.last_batch = None
         self.emitters = emitters
 
-        self.asked = False
-        self.solutions = []
+        self._asked = False
+        self._solutions = []
 
     def ask(self):
-        if self.asked:
-            raise RuntimeError("You have called ask() twice in a row.")
+        """Generates a batch of solutions by calling ask() on all emitters.
 
-        self.asked = True
-        self.solutions = []
+        .. note:: The order of the solutions returned from this method is
+            important, so do not rearrange them.
+
+        Returns:
+            (n_solutions, dim) array: An array of n solutions to evaluate. Each
+            row contains a single solution.
+        Raises:
+            RuntimeError: You attempt to call this method again without first
+                calling :meth:`tell`.
+        """
+        if self._asked:
+            raise RuntimeError("You have called ask() twice in a row.")
+        self._asked = True
+
+        self._solutions = []
         for emitter in self.emitters:
-            self.solutions.append(emitter.ask())
-        self.solutions = np.concatenate(self.solutions)
-        return self.solutions
+            self._solutions.append(emitter.ask())
+        self._solutions = np.concatenate(self._solutions, axis=0)
+        return self._solutions
 
     def tell(self, objective_values, behavior_values):
-        if not self.asked:
+        """Returns objective and behavior values for solutions from :meth:`ask`.
+
+        .. note:: The objective values and behavior values must be in the same
+            order as the solutions created by :meth:`ask`; i.e.
+            ``objective_values[i]`` and ``behavior_values[i]`` should be the
+            objective value and behavior values for ``solutions[i]``.
+
+        Args:
+            objective_values ((n_solutions,) array): Each entry of this array
+                contains the objective function evaluation of a solution.
+            behavior_values ((n_solutions, behavior_dm) array): Each row of
+                this array contains a solution's coordinates in behavior space.
+        Raises:
+            RuntimeError: You attempt to call this method without first calling
+                :meth:`ask`.
+        """
+        if not self._asked:
             raise RuntimeError("You have called tell() without ask().")
+        self._asked = False
 
-        self.asked = False
-
-        # Convert user input into numpy arrays
         objective_values = np.array(objective_values)
         behavior_values = np.array(behavior_values)
 
+        # Keep track of pos because emitters may have different batch sizes.
         pos = 0
         for emitter in self.emitters:
             end = pos + emitter.batch_size
-            emitter.tell(self.solutions[pos:end], objective_values[pos:end],
+            emitter.tell(self._solutions[pos:end], objective_values[pos:end],
                          behavior_values[pos:end])
             pos = end
