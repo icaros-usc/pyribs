ae40d2b130d5a0af6fd4d4f96c5a74f3ac2c0b7f
btjanaka <bryon@btjanaka.net>
2020-11-13 02:35:29 -0800

    Initial IsoLineEmitter

--- ribs/emitters/__init__.py
+++ ribs/emitters/__init__.py
@@ -1,8 +1,12 @@
 """Emitters output new candidate solutions in QD algorithms."""
 from ribs.emitters._gaussian_emitter import (GaussianEmitter,
                                              GaussianEmitterConfig)
+from ribs.emitters._iso_line_emitter import (IsoLineEmitter,
+                                             IsoLineEmitterConfig)
 
 __all__ = [
     "GaussianEmitter",
     "GaussianEmitterConfig",
+    "IsoLineEmitter",
+    "IsoLineEmitterConfig",
 ]
