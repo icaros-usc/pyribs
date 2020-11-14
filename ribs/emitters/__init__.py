288f07a812d8747a994377e5faec5b20c507e5de
btjanaka <bryon@btjanaka.net>
2020-11-13 20:56:12 -0800

    Add EmitterBase

--- ribs/emitters/__init__.py
+++ ribs/emitters/__init__.py
@@ -1,4 +1,5 @@
 """Emitters output new candidate solutions in QD algorithms."""
+from ribs.emitters._emitter_base import EmitterBase
 from ribs.emitters._gaussian_emitter import (GaussianEmitter,
                                              GaussianEmitterConfig)
 from ribs.emitters._iso_line_emitter import (IsoLineEmitter,
@@ -9,4 +10,5 @@ __all__ = [
     "GaussianEmitterConfig",
     "IsoLineEmitter",
     "IsoLineEmitterConfig",
+    "EmitterBase",
 ]
