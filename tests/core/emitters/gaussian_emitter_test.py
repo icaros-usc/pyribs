1edb8bee2948738b5888898b2a82267baed8e953
btjanaka <bryon@btjanaka.net>
2020-11-16 01:49:19 -0800

    Add tests for Gaussian Emitter

--- tests/core/emitters/gaussian_emitter_test.py
+++ tests/core/emitters/gaussian_emitter_test.py
@@ -1,4 +1,26 @@
 """Tests for the GaussianEmitter."""
+import numpy as np
+
 from ribs.emitters import GaussianEmitter
 
 # pylint: disable = invalid-name
+
+
+def test_degenerate_gaussian_emits_identical_sols(_archive_fixture):
+    archive, x0 = _archive_fixture
+    emitter = GaussianEmitter(x0, 0, archive, config={"batch_size": 2})
+    solutions = emitter.ask()
+    assert (solutions == np.expand_dims(x0, axis=0)).all()
+
+
+def test_degenerate_gaussian_emits_parent(_archive_fixture):
+    archive, x0 = _archive_fixture
+    parent_sol = x0 * 5
+    archive.add(parent_sol, 1, np.array([0, 0]))
+    emitter = GaussianEmitter(x0, 0, archive, config={"batch_size": 2})
+
+    # All solutions should be generated "around" the single parent solution in
+    # the archive.
+    solutions = emitter.ask()
+
+    assert (solutions == np.expand_dims(parent_sol, axis=0)).all()
