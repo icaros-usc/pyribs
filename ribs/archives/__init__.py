8ba27ac7a18f34a50352fa83e4d3ba7b9c43b0e6
btjanaka <bryon@btjanaka.net>
2020-11-20 16:45:55 -0800

    Move base classes to end of module listing

--- ribs/archives/__init__.py
+++ ribs/archives/__init__.py
@@ -10,9 +10,9 @@ from ribs.archives._cvt_archive import CVTArchive, CVTArchiveConfig
 from ribs.archives._grid_archive import GridArchive, GridArchiveConfig
 
 __all__ = [
-    "ArchiveBase",
     "CVTArchive",
     "CVTArchiveConfig",
     "GridArchive",
     "GridArchiveConfig",
+    "ArchiveBase",
 ]
