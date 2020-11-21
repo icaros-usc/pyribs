c2edfa88f4e56487da30b530e163ce4a94243ee6
btjanaka <bryon@btjanaka.net>
2020-11-20 20:44:06 -0800

    Add edit on github link to docs

--- docs/conf.py
+++ docs/conf.py
@@ -117,8 +117,8 @@ html_title = "pyribs v" + ribs.__version__
 # material theme options (see theme.conf for more information)
 html_theme_options = {
     "nav_title": "pyribs",
-    "base_url": "https://ribs.readthedocs.io",
-    "repo_url": "https://github.com/icaros-usc/pyribs",
+    "base_url": "https://ribs.readthedocs.io/",
+    "repo_url": "https://github.com/icaros-usc/pyribs/",
     "repo_name": "pyribs",
     "google_analytics_account": None,
     "html_minify": not DEV_MODE,
