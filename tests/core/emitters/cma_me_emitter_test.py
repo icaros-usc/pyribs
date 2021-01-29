b59e6df45399aacaf686723695f6f3ed4bf9c760
Bryon Tjanaka <bryon@btjanaka.net>
2021-01-28 20:47:14 -0800

    Add test for emitters

--- tests/core/emitters/cma_me_emitter_test.py
+++ tests/core/emitters/cma_me_emitter_test.py
@@ -33,3 +33,22 @@ def test_auto_batch_size(emitter_name):
 
     assert emitter.batch_size is not None
     assert isinstance(emitter.batch_size, int)
+
+
+@pytest.mark.parametrize(
+    "emitter_name",
+    ["ImprovementEmitter", "RandomDirectionEmitter", "OptimizingEmitter"])
+def test_list_as_initial_solution(emitter_name):
+    archive = GridArchive([20, 20], [(-1.0, 1.0)] * 2)
+
+    emitter = {
+        "ImprovementEmitter":
+            lambda: ImprovementEmitter(archive, [0.0] * 20, 1.0),
+        "RandomDirectionEmitter":
+            lambda: RandomDirectionEmitter(archive, [0.0] * 20, 1.0),
+        "OptimizingEmitter":
+            lambda: OptimizingEmitter(archive, [0.0] * 20, 1.0),
+    }[emitter_name]()
+
+    # The list was passed in but should be converted to a numpy array.
+    assert (emitter.x0 == np.zeros(20)).all()
