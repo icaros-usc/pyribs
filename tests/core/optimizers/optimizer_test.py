1f11f764892c2151677b0ca1d9b3d7a1041147a4
Bryon Tjanaka <bryon@btjanaka.net>
2021-02-02 22:07:39 -0800

    Move and rename test

--- tests/core/optimizers/optimizer_test.py
+++ tests/core/optimizers/optimizer_test.py
@@ -23,6 +23,17 @@ def test_init_fails_with_no_emitters():
         Optimizer(archive, emitters)
 
 
+def test_init_fails_on_non_unique_emitter_instances():
+    archive = GridArchive([100, 100], [(-1, 1), (-1, 1)])
+
+    # All emitters are the same object. This is bad because the same emitter
+    # gets called multiple times.
+    emitters = [GaussianEmitter(archive, [0.0, 0.0], 1, batch_size=1)] * 5
+
+    with pytest.raises(ValueError):
+        Optimizer(archive, emitters)
+
+
 def test_init_fails_with_mismatched_emitters():
     archive = GridArchive([100, 100], [(-1, 1), (-1, 1)])
     emitters = [
@@ -88,14 +99,3 @@ def test_tell_fails_when_ask_not_called(_optimizer_fixture):
     optimizer, _ = _optimizer_fixture
     with pytest.raises(RuntimeError):
         optimizer.tell(None, None)
-
-
-def test_using_the_same_emitter_fails():
-    archive = GridArchive([100, 100], [(-1, 1), (-1, 1)])
-
-    # All emitters are the same object. This is bad because the same emitter
-    # gets called multiple times.
-    emitters = [GaussianEmitter(archive, [0.0, 0.0], 1, batch_size=1)] * 5
-
-    with pytest.raises(ValueError):
-        Optimizer(archive, emitters)
