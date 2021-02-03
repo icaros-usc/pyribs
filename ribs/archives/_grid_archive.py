fa5516e41744da661aa51fad63c9bcef654ab37a
Bryon Tjanaka <bryon@btjanaka.net>
2021-02-03 10:48:07 -0800

    Edits

--- ribs/archives/_grid_archive.py
+++ ribs/archives/_grid_archive.py
@@ -88,7 +88,7 @@ class GridArchive(ArchiveBase):
             Archive bins:   | 0 | 1 |   ...   |    self.dims[i]    |
             boundaries[i]:  0   1   2   self.dims[i] - 1     self.dims[i]
 
-        Thus, entry ``j`` and ``j + 1`` are the lower and upper bounds of bin
+        Thus, entries ``j`` and ``j + 1`` are the lower and upper bounds of bin
         ``j``. To access the lower bounds of all the cells in dimension ``i``,
         use ``boundaries[i][:-1]``, and to access all the upper bounds, use
         ``boundaries[i][1:]``.
