4c3e18fcf8b3fe9ec6f839101a886687f5e2a034
Bryon Tjanaka <bryon@btjanaka.net>
2021-04-10 17:00:07 -0700

    Use data in archive tests and rename _data to data

--- tests/archives/sliding_boundaries_archive_test.py
+++ tests/archives/sliding_boundaries_archive_test.py
@@ -5,21 +5,15 @@ import pytest
 from ribs.archives import AddStatus, SlidingBoundariesArchive
 
 from .conftest import get_archive_data
+from .grid_archive_test import assert_archive_entry
+
+# pylint: disable = redefined-outer-name
 
 
 @pytest.fixture
-def _data():
+def data():
     """Data for sliding boundary archive tests."""
     return get_archive_data("SlidingBoundariesArchive")
-
-
-def _assert_archive_has_entry(archive, indices, behavior_values,
-                              objective_value, solution):
-    """Assert that the archive has one specific entry."""
-    archive_data = archive.as_pandas()
-    assert len(archive_data) == 1
-    assert (archive_data.iloc[0] == (list(indices) + list(behavior_values) +
-                                     [objective_value] + list(solution))).all()
 
 
 def test_fails_on_dim_mismatch():
@@ -30,67 +24,72 @@ def test_fails_on_dim_mismatch():
         )
 
 
-def test_attributes_correctly_constructed(_data):
-    assert np.all(_data.archive.dims == [10, 20])
-    assert np.all(_data.archive.lower_bounds == [-1, -2])
-    assert np.all(_data.archive.upper_bounds == [1, 2])
-    assert np.all(_data.archive.interval_size == [2, 4])
+def test_attributes_correctly_constructed(data):
+    assert np.all(data.archive.dims == [10, 20])
+    assert np.all(data.archive.lower_bounds == [-1, -2])
+    assert np.all(data.archive.upper_bounds == [1, 2])
+    assert np.all(data.archive.interval_size == [2, 4])
 
     # Check the shape of boundaries.
-    assert len(_data.archive.boundaries[0]) == 11
-    assert len(_data.archive.boundaries[1]) == 21
-    assert _data.archive.remap_frequency == 100
-    assert _data.archive.buffer_capacity == 1000
+    assert len(data.archive.boundaries[0]) == 11
+    assert len(data.archive.boundaries[1]) == 21
+    assert data.archive.remap_frequency == 100
+    assert data.archive.buffer_capacity == 1000
 
 
 @pytest.mark.parametrize("use_list", [True, False], ids=["list", "ndarray"])
-def test_add_to_archive(_data, use_list):
+def test_add_to_archive(data, use_list):
     if use_list:
-        status, value = _data.archive.add(list(_data.solution),
-                                          _data.objective_value,
-                                          list(_data.behavior_values))
+        status, value = data.archive.add(list(data.solution),
+                                         data.objective_value,
+                                         list(data.behavior_values),
+                                         data.metadata)
     else:
-        status, value = _data.archive.add(_data.solution, _data.objective_value,
-                                          _data.behavior_values)
+        status, value = data.archive.add(data.solution, data.objective_value,
+                                         data.behavior_values, data.metadata)
 
     assert status == AddStatus.NEW
-    assert np.isclose(value, _data.objective_value)
-    _assert_archive_has_entry(_data.archive_with_entry, _data.grid_indices,
-                              _data.behavior_values, _data.objective_value,
-                              _data.solution)
+    assert np.isclose(value, data.objective_value)
+    assert_archive_entry(data.archive_with_entry, data.solution,
+                         data.objective_value, data.behavior_values,
+                         data.grid_indices, data.metadata)
 
 
-def test_add_and_overwrite(_data):
+def test_add_and_overwrite(data):
     """Test adding a new entry with a higher objective value."""
-    arbitrary_sol = _data.solution + 1
-    high_objective_value = _data.objective_value + 1.0
+    arbitrary_sol = data.solution + 1
+    arbitrary_metadata = {"foobar": 12}
+    high_objective_value = data.objective_value + 1.0
 
-    status, value = _data.archive_with_entry.add(arbitrary_sol,
-                                                 high_objective_value,
-                                                 _data.behavior_values)
+    status, value = data.archive_with_entry.add(arbitrary_sol,
+                                                high_objective_value,
+                                                data.behavior_values,
+                                                arbitrary_metadata)
     assert status == AddStatus.IMPROVE_EXISTING
-    assert np.isclose(value, high_objective_value - _data.objective_value)
-    _assert_archive_has_entry(_data.archive_with_entry, _data.grid_indices,
-                              _data.behavior_values, high_objective_value,
-                              arbitrary_sol)
+    assert np.isclose(value, high_objective_value - data.objective_value)
+    assert_archive_entry(data.archive_with_entry, arbitrary_sol,
+                         high_objective_value, data.behavior_values,
+                         data.grid_indices, arbitrary_metadata)
 
 
-def test_add_without_overwrite(_data):
+def test_add_without_overwrite(data):
     """Test adding a new entry with a lower objective value."""
-    arbitrary_sol = _data.solution + 1
-    low_objective_value = _data.objective_value - 1.0
+    arbitrary_sol = data.solution + 1
+    arbitrary_metadata = {"foobar": 12}
+    low_objective_value = data.objective_value - 1.0
 
-    status, value = _data.archive_with_entry.add(arbitrary_sol,
-                                                 low_objective_value,
-                                                 _data.behavior_values)
+    status, value = data.archive_with_entry.add(arbitrary_sol,
+                                                low_objective_value,
+                                                data.behavior_values,
+                                                arbitrary_metadata)
     assert status == AddStatus.NOT_ADDED
-    assert np.isclose(value, low_objective_value - _data.objective_value)
-    _assert_archive_has_entry(_data.archive_with_entry, _data.grid_indices,
-                              _data.behavior_values, _data.objective_value,
-                              _data.solution)
+    assert np.isclose(value, low_objective_value - data.objective_value)
+    assert_archive_entry(data.archive_with_entry, data.solution,
+                         data.objective_value, data.behavior_values,
+                         data.grid_indices, data.metadata)
 
 
-def test_initial_remap(_data):
+def test_initial_remap():
     """Checks that boundaries and entries are correct after initial remap."""
     # remap_frequency is (10 + 1) * (20 + 1)
     archive = SlidingBoundariesArchive([10, 20], [(-1, 1), (-2, 2)],
@@ -143,26 +142,28 @@ def test_initial_remap(_data):
     assert np.isclose(sorted(bcs), sorted(expected_bcs)).all()
 
 
-def test_add_to_archive_with_full_buffer(_data):
-    for _ in range(_data.archive.buffer_capacity + 1):
-        _data.archive.add(_data.solution, _data.objective_value,
-                          _data.behavior_values)
+def test_add_to_archive_with_full_buffer(data):
+    for _ in range(data.archive.buffer_capacity + 1):
+        data.archive.add(data.solution, data.objective_value,
+                         data.behavior_values, data.metadata)
 
     # After adding the same entry multiple times, there should only be one
     # entry, and it should be at (0, 0).
-    _assert_archive_has_entry(_data.archive, (0, 0), _data.behavior_values,
-                              _data.objective_value, _data.solution)
+    assert_archive_entry(data.archive, data.solution, data.objective_value,
+                         data.behavior_values, (0, 0), data.metadata)
 
     # Even if another entry is added, it should still go to the same cell
     # because the behavior values are clipped to the boundaries before being
     # inserted.
-    _data.archive.add(2 * _data.solution, 2 * _data.objective_value,
-                      2 * _data.behavior_values)
-    _assert_archive_has_entry(_data.archive, (0, 0), 2 * _data.behavior_values,
-                              2 * _data.objective_value, 2 * _data.solution)
+    arbitrary_metadata = {"foobar": 12}
+    data.archive.add(2 * data.solution, 2 * data.objective_value,
+                     2 * data.behavior_values, arbitrary_metadata)
+    assert_archive_entry(data.archive, 2 * data.solution,
+                         2 * data.objective_value, 2 * data.behavior_values,
+                         (0, 0), arbitrary_metadata)
 
 
-def test_adds_solutions_from_old_archive(_data):
+def test_adds_solutions_from_old_archive():
     """Solutions from previous archive should be inserted during remap."""
     archive = SlidingBoundariesArchive([10, 20], [(-1, 1), (-2, 2)],
                                        remap_frequency=231,
