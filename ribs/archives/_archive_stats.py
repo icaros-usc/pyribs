79e924c4821674a6cf2d9934123848e40e5be67b
Bryon Tjanaka <38124174+btjanaka@users.noreply.github.com>
2021-07-07 13:12:40 -0700

    Move archive statistics to namedtuple attribute (#157)
    - Add namedtuple
    - Move stats to namedtuple
    - Fix current usages

---
+++ ribs/archives/_archive_stats.py
@@ -0,0 +1,31 @@
+"""Provides ArchiveStats."""
+from typing import NamedTuple
+
+import numpy as np
+
+
+class ArchiveStats(NamedTuple):
+    """Holds statistics about an archive.
+
+    Attributes of type :class:`~numpy.floating` will match the
+    :attr:`~ArchiveBase.dtype` of their archive.
+    """
+
+    #: Number of elites in the archive.
+    elites: int
+
+    #: Proportion of bins in the archive that have an elite - always in the
+    #: range :math:`[0,1]`.
+    coverage: np.floating
+
+    #: QD score, i.e. sum of objective values of all elites in the archive.
+    #: **This score only makes sense if objective values are non-negative.**
+    qd_score: np.floating
+
+    #: Maximum objective value of the elites in the archive. None if there are
+    #: no elites in the archive.
+    obj_max: np.floating
+
+    #: Mean objective value of the elites in the archive. None if there are no
+    #: elites in the archive.
+    obj_mean: np.floating
