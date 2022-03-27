fa5206737e06819f6d3e9377769b4bd6908c5871
Bryon Tjanaka <bryon@btjanaka.net>
2022-03-27 16:32:01 -0700

    Remove get_random_elite benchmarks

--- tests/archives/cvt_archive_benchmark.py
+++ tests/archives/cvt_archive_benchmark.py
@@ -37,21 +37,6 @@ def benchmark_add_10k(use_kd_tree, benchmark, benchmark_data_10k):
     benchmark.pedantic(add_10k, setup=setup, rounds=5, iterations=1)
 
 
-def benchmark_get_10k_random_elites(use_kd_tree, benchmark, benchmark_data_10k):
-    n, solutions, objective_values, behavior_values = benchmark_data_10k
-    archive = CVTArchive(1000, [(-1, 1), (-1, 1)],
-                         samples=20_000,
-                         use_kd_tree=use_kd_tree)
-    archive.initialize(solutions.shape[1])
-    for i in range(n):
-        archive.add(solutions[i], objective_values[i], behavior_values[i])
-
-    def get_elites():
-        archive.sample_elites(n)
-
-    benchmark(get_elites)
-
-
 def benchmark_as_pandas_2000_items(benchmark):
     cells = 2000
     archive = CVTArchive(cells, [(-1, 1), (-1, 1)],
