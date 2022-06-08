3f233071a305e5d204c3335706e94977201a88ad
Elle Szabo <eszabo@usc.edu>
2022-06-08 15:31:04 -0700

    removed underscore

--- ribs/archives/_archive_base.py
+++ ribs/archives/_archive_base.py
@@ -101,7 +101,7 @@ class ArchiveBase(ABC):  # pylint: disable = too-many-instance-attributes
         accessed by child classes (i.e. they are "protected" attributes).
 
     Args:
-        _solution_dim (int): See ``solution_dim`` arg.
+        solution_dim (int): See ``solution_dim`` arg.
         cells (int): Number of cells in the archive. This is used to create the
             numpy arrays described above for storing archive info.
         behavior_dim (int): The dimension of the behavior space.
