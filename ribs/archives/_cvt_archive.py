e6996ed4086c80b82982c98f9c506ea7139a5ee5
Bryon Tjanaka <bryon@btjanaka.net>
2022-07-06 17:28:42 -0700

    Formatting

--- ribs/archives/_cvt_archive.py
+++ ribs/archives/_cvt_archive.py
@@ -78,8 +78,8 @@ class CVTArchive(ArchiveBase):
             ``archive.samples`` will be None. This can be useful when one wishes
             to use the same CVT across experiments for fair comparison.
         k_means_kwargs (dict): kwargs for :func:`~sklearn.cluster.k_means`. By
-            default, we pass in `n_init=1`, `init="random"`, `algorithm="lloyd"`,
-            and `random_state=seed`.
+            default, we pass in `n_init=1`, `init="random"`,
+            `algorithm="lloyd"`, and `random_state=seed`.
         use_kd_tree (bool): If True, use a k-D tree for finding the closest
             centroid when inserting into the archive. This may result in a
             speedup for larger dimensions.
@@ -128,7 +128,8 @@ class CVTArchive(ArchiveBase):
         if "algorithm" not in self._k_means_kwargs:
             if semantic_version.Version(
                     sklearn.__version__) >= semantic_version.Version("1.1.0"):
-                # In the newer versions, "full" has been deprecated in favor of "lloyd".
+                # In the newer versions, "full" has been deprecated in favor of
+                # "lloyd".
                 self._k_means_kwargs["algorithm"] = "lloyd"
             else:
                 # The default, "auto"/"elkan", allocates a huge array.
