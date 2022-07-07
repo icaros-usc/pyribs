c438146d5c29ac05b14ec8a28d62e7f8c07da849
Bryon Tjanaka <bryon@btjanaka.net>
2022-07-06 17:28:09 -0700

    Allow extra msg in error funcs

--- ribs/_utils.py
+++ ribs/_utils.py
@@ -1,7 +1,7 @@
 """Miscellaneous internal utilities."""
 
 
-def check_batch_shape(array, array_name, dim, dim_name):
+def check_batch_shape(array, array_name, dim, dim_name, extra_msg=""):
     """Checks that the array has shape (batch_size, dim).
 
     `array` must be a numpy array, and `dim` must be an int.
@@ -10,10 +10,10 @@ def check_batch_shape(array, array_name, dim, dim_name):
         raise ValueError(f"Expected {array_name} to be a 2D array with shape "
                          f"(batch_size, {dim}) (i.e. shape "
                          f"(batch_size, {dim_name})) but it had shape "
-                         f"{array.shape}")
+                         f"{array.shape}{extra_msg}")
 
 
-def check_1d_shape(array, array_name, dim, dim_name):
+def check_1d_shape(array, array_name, dim, dim_name, extra_msg=""):
     """Checks that the array has shape (dim,).
 
     `array` must be a numpy array, and `dim` must be an int.
@@ -21,4 +21,4 @@ def check_1d_shape(array, array_name, dim, dim_name):
     if array.ndim != 1 or array.shape[0] != dim:
         raise ValueError(f"Expected {array_name} to be a 1D array with shape "
                          f"({dim},) (i.e. shape ({dim_name},)) "
-                         f"but it had shape {array.shape}")
+                         f"but it had shape {array.shape}{extra_msg}")
