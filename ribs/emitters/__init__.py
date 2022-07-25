6747ccaff75b248ab2cc8edf79a676bff5f0684f
David Lee <dhlee@usc.edu>
2022-07-25 16:07:13 -0700

    add cma_mega emitter to module

--- ribs/emitters/__init__.py
+++ ribs/emitters/__init__.py
@@ -9,17 +9,21 @@
 .. autosummary::
     :toctree:
 
+    ribs.emitters.GradientAborescenceEmitter
     ribs.emitters.EvolutionStrategyEmitter
     ribs.emitters.GaussianEmitter
     ribs.emitters.IsoLineEmitter
     ribs.emitters.EmitterBase
 """
 from ribs.emitters._emitter_base import EmitterBase
-from ribs.emitters._gaussian_emitter import GaussianEmitter
-from ribs.emitters._iso_line_emitter import IsoLineEmitter
 from ribs.emitters._evolution_strategy_emitter import EvolutionStrategyEmitter
+from ribs.emitters._gaussian_emitter import GaussianEmitter
+from ribs.emitters._gradient_aborescence_emitter import \
+  GradientAborescenceEmitter
+from ribs.emitters._iso_line_emitter import IsoLineEmitter
 
 __all__ = [
+    "GradientAborescenceEmitter",
     "EvolutionStrategyEmitter",
     "GaussianEmitter",
     "IsoLineEmitter",
