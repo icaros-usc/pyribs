d1598a1339d4bbb2555e3b8aea1f89490638267d
David Lee <dhlee@usc.edu>
2022-08-10 17:46:35 -0700

    add tests

--- tests/emitters/evolution_strategy_emitter_test.py
+++ tests/emitters/evolution_strategy_emitter_test.py
@@ -43,3 +43,21 @@ def test_dtypes(dtype):
                                                 measures_batch)
         emitter.tell(solution_batch, objective_batch, measures_batch,
                      status_batch, value_batch)
+
+
+def test_simga0_vector():
+    ranker = get_ranker("obj")
+    archive = GridArchive(10, [20, 20], [(-1.0, 1.0)] * 2)
+    emitter = EvolutionStrategyEmitter(archive, np.zeros(10), [1.0, 1.0],
+                                       ranker)
+
+    # Try running with the negative sphere function for a few iterations.
+    for _ in range(10):
+        solution_batch = emitter.ask()
+        objective_batch = -np.sum(np.square(solution_batch), axis=1)
+        measures_batch = solution_batch[:, :2]
+
+        status_batch, value_batch = archive.add(solution_batch, objective_batch,
+                                                measures_batch)
+        emitter.tell(solution_batch, objective_batch, measures_batch,
+                     status_batch, value_batch)
