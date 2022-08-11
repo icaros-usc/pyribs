6a136ab92449b7bf2013513f9ded2bca263afb6b
David Lee <dhlee@usc.edu>
2022-08-10 17:45:52 -0700

    Update emitter tell() docstring to no longer say “Inserts entries into archive” (#247)
    * docstrings
    
    * hi
    story
    
    * history

--- ribs/emitters/_evolution_strategy_emitter.py
+++ ribs/emitters/_evolution_strategy_emitter.py
@@ -156,13 +156,12 @@ class EvolutionStrategyEmitter(EmitterBase):
              metadata_batch=None):
         """Gives the emitter results from evaluating solutions.
 
-        As we insert solutions into the archive, we record the solutions'
-        impact on the fitness of the archive. For example, if the added
-        solution makes an improvement on an existing elite, then we
-        will record ``(AddStatus.IMPROVED_EXISTING, improvement_value)``
-
         The solutions are ranked based on the `rank()` function defined by
-        `self._ranker`.
+        `self._ranker`. Then, the ranked solutions are passed to CMA-ES for
+        adaptation.
+
+        This function also checks for restart condition and restarts CMA-ES
+        when needed.
 
         Args:
             solution_batch (numpy.ndarray): (batch_size, :attr:`solution_dim`)
