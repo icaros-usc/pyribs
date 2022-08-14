icaros-usc/pyribs

commit feb3873fb7236ec23144f50546c7e71776fce6e4

Implement CMA-MEGA (#240)

ribs/emitters/opt/_cma_es.py
@@ -154,7 +154,7 @@ class CMAEvolutionStrategy:
             return True
 
         # Area of distribution too small.
-        area = self.sigma * np.sqrt(max(self.cov.eigenvalues))
+        area = self.sigma * np.sqrt(np.max(self.cov.eigenvalues))
         if area < 1e-11:
             return True
 
@@ -231,7 +231,7 @@ class CMAEvolutionStrategy:
                        np.log(np.arange(1, num_parents + 1)))
             total_weights = np.sum(weights)
             weights = weights / total_weights
-            mueff = np.sum(weights)**2 / np.sum(weights**2)
+            mueff = total_weights**2 / np.sum(weights**2)
         elif self.weight_rule == "active":
             weights = None
 

examples/sphere.py
@@ -30,6 +30,13 @@ The supported algorithms are:
 - `cma_me_mixed`: GridArchive with EvolutionStrategyEmitter, where half (7) of
   the emitter are using TwoStageRandomDirectionRanker and half (8) are
   TwoStageImprovementRanker.
+- `cma_mega`: GridArchive with GradientAborescenceEmitter.
+- `cma_mega_adam`: GridArchive with GradientAborescenceEmitter using Adam
+  Optimizer.
+
+Note: the settings for `cma_mega` and `cma_mega_adam` are consistent with the
+paper (`Fontaine 2021 <https://arxiv.org/abs/2106.03894>`_) in which these
+algorithms are proposed.
 
 All algorithms use 15 emitters, each with a batch size of 37. Each one runs for
 4500 iterations for a total of 15 * 37 * 4500 ~= 2.5M evaluations.
@@ -74,21 +81,21 @@ from alive_progress import alive_bar
 
 from ribs.archives import CVTArchive, GridArchive
 from ribs.emitters import (EvolutionStrategyEmitter, GaussianEmitter,
-                           IsoLineEmitter)
+                           GradientAborescenceEmitter, IsoLineEmitter)
 from ribs.optimizers import Optimizer
 from ribs.visualize import cvt_archive_heatmap, grid_archive_heatmap
 
 
-def sphere(sol):
+def sphere(solution_batch):
     """Sphere function evaluation and BCs for a batch of solutions.
 
     Args:
-        sol (np.ndarray): (batch_size, dim) array of solutions
+        solution_batch (np.ndarray): (batch_size, dim) array of solutions
     Returns:
-        objs (np.ndarray): (batch_size,) array of objective values
-        bcs (np.ndarray): (batch_size, 2) array of behavior values
+        objective_batch (np.ndarray): (batch_size,) array of objective values
+        measures_batch (np.ndarray): (batch_size, 2) array of measure values
     """
-    dim = sol.shape[1]
+    dim = solution_batch.shape[1]
 
     # Shift the Sphere function so that the optimal value is at x_i = 2.048.
     sphere_shift = 5.12 * 0.4
@@ -96,14 +103,17 @@ def sphere(sol):
     # Normalize the objective to the range [0, 100] where 100 is optimal.
     best_obj = 0.0
     worst_obj = (-5.12 - sphere_shift)**2 * dim
-    raw_obj = np.sum(np.square(sol - sphere_shift), axis=1)
-    objs = (raw_obj - worst_obj) / (best_obj - worst_obj) * 100
+    raw_obj = np.sum(np.square(solution_batch - sphere_shift), axis=1)
+    objective_batch = (raw_obj - worst_obj) / (best_obj - worst_obj) * 100
 
-    # Calculate BCs.
-    clipped = sol.copy()
+    # Compute gradient of the objective
+    objective_grad_batch = -2 * (solution_batch - sphere_shift)
+
+    # Calculate measures.
+    clipped = solution_batch.copy()
     clip_indices = np.where(np.logical_or(clipped > 5.12, clipped < -5.12))
     clipped[clip_indices] = 5.12 / clipped[clip_indices]
-    bcs = np.concatenate(
+    measures_batch = np.concatenate(
         (
             np.sum(clipped[:, :dim // 2], axis=1, keepdims=True),
             np.sum(clipped[:, dim // 2:], axis=1, keepdims=True),
@@ -111,7 +121,24 @@ def sphere(sol):
         axis=1,
     )
 
-    return objs, bcs
+    # Compute gradient of the measures
+    derivatives = np.ones(solution_batch.shape)
+    derivatives[clip_indices] = -5.12 / np.square(solution_batch[clip_indices])
+
+    mask_0 = np.concatenate((np.ones(dim // 2), np.zeros(dim - dim // 2)))
+    mask_1 = np.concatenate((np.zeros(dim // 2), np.ones(dim - dim // 2)))
+
+    d_measure0 = derivatives * mask_0
+    d_measure1 = derivatives * mask_1
+
+    measures_grad_batch = np.stack((d_measure0, d_measure1), axis=1)
+
+    return (
+        objective_batch,
+        objective_grad_batch,
+        measures_batch,
+        measures_grad_batch,
+    )
 
 
 def create_optimizer(algorithm, dim, seed):
@@ -145,6 +172,13 @@ def create_optimizer(algorithm, dim, seed):
                              ranges=bounds,
                              samples=100_000,
                              use_kd_tree=True)
+    elif algorithm in ["cma_mega", "cma_mega_adam"]:
+        # Note that the archive is smaller for these algorithms. This is to be
+        # consistent with Fontaine 2021 <https://arxiv.org/abs/2106.03894>.
+        archive = GridArchive(solution_dim=dim,
+                              dims=(100, 100),
+                              ranges=bounds,
+                              seed=seed)
     else:
         raise ValueError(f"Algorithm `{algorithm}` is not recognized")
 
@@ -213,7 +247,36 @@ def create_optimizer(algorithm, dim, seed):
                 seed=s,
             ) for s in emitter_seeds
         ]
-
+    elif algorithm == "cma_mega":
+        # Note that only one emitter is used for cma_mega. This is to be
+        # consistent with Fontaine 2021 <https://arxiv.org/abs/2106.03894>.
+        emitters = [
+            GradientAborescenceEmitter(
+                archive,
+                initial_sol,
+                sigma0=10.0,
+                step_size=1.0,
+                grad_opt="gradient_ascent",
+                selection_rule="mu",
+                bounds=None,
+                batch_size=batch_size - 1,  # 1 solution is returned by ask_dqd
+                seed=emitter_seeds[0])
+        ]
+    elif algorithm == "cma_mega_adam":
+        # Note that only one emitter is used for cma_mega_adam. This is to be
+        # consistent with Fontaine 2021 <https://arxiv.org/abs/2106.03894>.
+        emitters = [
+            GradientAborescenceEmitter(
+                archive,
+                initial_sol,
+                sigma0=10.0,
+                step_size=0.002,
+                grad_opt="adam",
+                selection_rule="mu",
+                bounds=None,
+                batch_size=batch_size - 1,  # 1 solution is returned by ask_dqd
+                seed=emitter_seeds[0])
+        ]
     return Optimizer(archive, emitters)
 
 
@@ -238,8 +301,8 @@ def save_heatmap(archive, heatmap_path):
 
 
 def sphere_main(algorithm,
-                dim=20,
-                itrs=4500,
+                dim=None,
+                itrs=None,
                 outdir="sphere_output",
                 log_freq=250,
                 seed=None):
@@ -254,10 +317,30 @@ def sphere_main(algorithm,
             and saving heatmap.
         seed (int): Seed for the algorithm. By default, there is no seed.
     """
+    if dim is None:
+        if algorithm in ["cma_mega", "cma_mega_adam"]:
+            dim = 1_000
+        elif algorithm in [
+                "map_elites", "line_map_elites", "cma_me_imp", "cma_me_imp_mu",
+                "cma_me_rd", "cma_me_rd_mu", "cma_me_opt", "cma_me_mixed"
+        ]:
+            dim = 20
+
+    if itrs is None:
+        if algorithm in ["cma_mega", "cma_mega_adam"]:
+            itrs = 10_000
+        elif algorithm in [
+                "map_elites", "line_map_elites", "cma_me_imp", "cma_me_imp_mu",
+                "cma_me_rd", "cma_me_rd_mu", "cma_me_opt", "cma_me_mixed"
+        ]:
+            itrs = 4500
+
     name = f"{algorithm}_{dim}"
     outdir = Path(outdir)
     if not outdir.is_dir():
         outdir.mkdir()
+
+    is_dqd = algorithm in ['cma_mega', 'cma_mega_adam']
 
     optimizer = create_optimizer(algorithm, dim, seed)
     archive = optimizer.archive
@@ -278,9 +361,21 @@ def sphere_main(algorithm,
 
         for itr in range(1, itrs + 1):
             itr_start = time.time()
-            sols = optimizer.ask()
-            objs, bcs = sphere(sols)
-            optimizer.tell(objs, bcs)
+
+            if is_dqd:
+                solution_batch = optimizer.ask_dqd()
+                (objective_batch, objective_grad_batch, measures_batch,
+                 measures_grad_batch) = sphere(solution_batch)
+                objective_grad_batch = np.expand_dims(objective_grad_batch,
+                                                      axis=1)
+                jacobian_batch = np.concatenate(
+                    (objective_grad_batch, measures_grad_batch), axis=1)
+                optimizer.tell_dqd(objective_batch, measures_batch,
+                                   jacobian_batch)
+
+            solution_batch = optimizer.ask()
+            objective_batch, _, measure_batch, _ = sphere(solution_batch)
+            optimizer.tell(objective_batch, measure_batch)
             non_logging_time += time.time() - itr_start
             progress()
 

ribs/emitters/__init__.py
@@ -1,5 +1,9 @@
 """Emitters output new candidate solutions in QD algorithms.
 
+All emitters should inherit from :class:`EmitterBase`, except for emitters
+designed for differentiable quality diversity (DQD), which should instead
+inherit from :class:`DQDEmitterBase`.
+
 .. note::
     Emitters provided here take on the data type of the archive passed to their
     constructor. For instance, if an archive has dtype ``np.float64``, then an
@@ -10,18 +14,25 @@
     :toctree:
 
     ribs.emitters.EvolutionStrategyEmitter
+    ribs.emitters.GradientAborescenceEmitter
     ribs.emitters.GaussianEmitter
     ribs.emitters.IsoLineEmitter
     ribs.emitters.EmitterBase
+    ribs.emitters.DQDEmitterBase
 """
+from ribs.emitters._dqd_emitter_base import DQDEmitterBase
 from ribs.emitters._emitter_base import EmitterBase
-from ribs.emitters._gaussian_emitter import GaussianEmitter
-from ribs.emitters._iso_line_emitter import IsoLineEmitter
 from ribs.emitters._evolution_strategy_emitter import EvolutionStrategyEmitter
+from ribs.emitters._gaussian_emitter import GaussianEmitter
+from ribs.emitters._gradient_aborescence_emitter import \
+  GradientAborescenceEmitter
+from ribs.emitters._iso_line_emitter import IsoLineEmitter
 
 __all__ = [
     "EvolutionStrategyEmitter",
+    "GradientAborescenceEmitter",
     "GaussianEmitter",
     "IsoLineEmitter",
     "EmitterBase",
+    "DQDEmitterBase",
 ]

ribs/emitters/_dqd_emitter_base.py
@@ -1,0 +1,58 @@
+"""Provides DQDEmitterBase."""
+from abc import abstractmethod
+
+from ribs.emitters._emitter_base import EmitterBase
+
+
+class DQDEmitterBase(EmitterBase):
+    """Base class for differentiable quality diversity (DQD) emitters.
+
+    This class is a special instance of :class:`EmitterBase` which implements
+    :meth:`ask_dqd` and :meth:`tell_dqd`. These two functions should be used to
+    communicate gradient information to the emitters. The ask and tell functions
+    should now be called in this order: :meth:`ask_dqd`, :meth:`tell_dqd`,
+    :meth:`ask`, :meth:`tell`.
+    """
+
+    @abstractmethod
+    def ask_dqd(self):
+        """Generates a ``(batch_size, solution_dim)`` array of solutions for
+        which gradient information must be computed."""
+
+    @abstractmethod
+    def tell_dqd(self,
+                 solution_batch,
+                 objective_batch,
+                 measures_batch,
+                 jacobian_batch,
+                 status_batch,
+                 value_batch,
+                 metadata_batch=None):
+        """Gives the emitter results from evaluating the gradient of the
+        solutions.
+
+        See :meth:`tell` for the definitions of the remaining arguments.
+
+        Args:
+            solution_batch (numpy.ndarray): (batch_size, :attr:`solution_dim`)
+                array of solutions generated by this emitter's :meth:`ask()`
+                method.
+            objective_batch (numpy.ndarray): 1d array containing the objective
+                function value of each solution.
+            measures_batch (numpy.ndarray): (batch_size, measure space
+                dimension) array with the measure space coordinates of each
+                solution.
+            jacobian_batch (numpy.ndarray): ``(batch_size, 1 + measure_dim,
+                solution_dim)`` array consisting of Jacobian matrices of the
+                solutions obtained from :meth:`ask_dqd`. Each matrix should
+                consist of the objective gradient of the solution followed by
+                the measure gradients.
+            status_batch (numpy.ndarray): 1d array of
+                :class:`ribs.archive.addstatus` returned by a series of calls to
+                archive's :meth:`add()` method.
+            value_batch (numpy.ndarray): 1d array of floats returned by a series
+                of calls to archive's :meth:`add()` method. for what these
+                floats represent, refer to :meth:`ribs.archives.add()`.
+            metadata_batch (numpy.ndarray): 1d object array containing a
+                metadata object for each solution.
+        """

ribs/emitters/_emitter_base.py
@@ -96,7 +96,7 @@ class EmitterBase(ABC):
 
     @abstractmethod
     def ask(self):
-        """Generates an ``(n, solution_dim)`` array of solutions."""
+        """Generates a ``(batch_size, solution_dim)`` array of solutions."""
 
     @abstractmethod
     def tell(self,

ribs/emitters/_gradient_aborescence_emitter.py
@@ -1,0 +1,327 @@
+"""Provides the GradientImprovementEmitter."""
+import itertools
+
+import numpy as np
+
+from ribs.emitters._dqd_emitter_base import DQDEmitterBase
+from ribs.emitters.opt import AdamOpt, CMAEvolutionStrategy, GradientAscentOpt
+from ribs.emitters.rankers import _get_ranker
+
+
+class GradientAborescenceEmitter(DQDEmitterBase):
+    """Generates solutions with a gradient arborescence, with coefficients
+    parameterized by CMA-ES.
+
+    This emitter originates in `Fontaine 2021
+    <https://arxiv.org/abs/2106.03894>`_. It leverages the gradient information
+    of the objective and measure functions, generating new solutions around a
+    "solution point" using gradient aborescence with coefficients drawn from a
+    Gaussian distribution. Based on how the solutions are ranked after being
+    inserted into the archive (see ``ranker``), the solution point is updated
+    with gradient ascent, and the distribution is updated with CMA-ES.
+
+    Note that unlike non-gradient emitters, GradientAborescenceEmitter requires
+    calling :meth:`ask_dqd` and :meth:`tell_dqd` (in this order) before calling
+    :meth:`ask` and :meth:`tell` to communicate the gradient information to the
+    emitter.
+
+    Args:
+        archive (ribs.archives.ArchiveBase): An archive to use when creating and
+            inserting solutions. For instance, this can be
+            :class:`ribs.archives.GridArchive`.
+        x0 (np.ndarray): Initial solution.
+        sigma0 (float): Initial step size / standard deviation.
+        step_size (float): Step size for the gradient optimizer
+        ranker (Callable or str): The ranker is a :class:`RankerBase` object
+            that orders the solutions after they have been evaluated in the
+            environment. This parameter may be a callable (e.g. a class or a
+            lambda function) that takes in no parameters and returns an instance
+            of :class:`RankerBase`, or it may be a full or abbreviated ranker
+            name as described in :meth:`ribs.emitters.rankers.get_ranker`.
+        selection_rule ("mu" or "filter"): Method for selecting parents in
+            CMA-ES. With "mu" selection, the first half of the solutions will be
+            selected as parents, while in "filter", any solutions that were
+            added to the archive will be selected.
+        restart_rule ("no_improvement" or "basic"): Method to use when checking
+            for restarts. With "basic", only the default CMA-ES convergence
+            rules will be used, while with "no_improvement", the emitter will
+            restart when none of the proposed solutions were added to the
+            archive.
+        grad_opt ("adam" or "gradient_ascent"): Gradient optimizer to use for
+            the gradient ascent step of the algorithm. Defaults to `adam`.
+        normalize_grad (bool): If true (default), then gradient infomation will
+            be normalized. Otherwise, it will not be normalized.
+        bounds (None or array-like): Bounds of the solution space. As suggested
+            in `Biedrzycki 2020
+            <https://www.sciencedirect.com/science/article/abs/pii/S2210650219301622>`_,
+            solutions are resampled until they fall within these bounds.  Pass
+            None to indicate there are no bounds. Alternatively, pass an
+            array-like to specify the bounds for each dim. Each element in this
+            array-like can be None to indicate no bound, or a tuple of
+            ``(lower_bound, upper_bound)``, where ``lower_bound`` or
+            ``upper_bound`` may be None to indicate no bound.
+        batch_size (int): Number of solutions to return in :meth:`ask`. If not
+            passed in, a batch size will be automatically calculated using the
+            default CMA-ES rules. Note that `batch_size` **does not** include
+            the number of solutions returned by :meth:`ask_dqd`, but also note
+            that :meth:`ask_dqd` always returns one solution, i.e. the solution
+            point.
+        epsilon (float): For numerical stability, we add a small epsilon when
+            normalizing gradients in :meth:`tell_dqd` -- refer to the
+            implementation `here
+            <../_modules/ribs/emitters/_gradient_aborescence_emitter.html#GradientAborescenceEmitter.tell_dqd>`_.
+            Pass this parameter to configure that epsilon.
+        seed (int): Value to seed the random number generator. Set to None to
+            avoid a fixed seed.
+    Raises:
+        ValueError: If ``restart_rule`` is invalid.
+    """
+
+    def __init__(self,
+                 archive,
+                 x0,
+                 sigma0,
+                 step_size,
+                 ranker="2imp",
+                 selection_rule="filter",
+                 restart_rule="no_improvement",
+                 grad_opt="adam",
+                 normalize_grad=True,
+                 bounds=None,
+                 batch_size=None,
+                 epsilon=1e-8,
+                 seed=None):
+        self._epsilon = epsilon
+        self._rng = np.random.default_rng(seed)
+        self._x0 = np.array(x0, dtype=archive.dtype)
+        self._sigma0 = sigma0
+        self._normalize_grads = normalize_grad
+        self._jacobian_batch = None
+        self._grad_coefficients = None
+        DQDEmitterBase.__init__(
+            self,
+            archive,
+            len(self._x0),
+            bounds,
+        )
+
+        self._ranker = _get_ranker(ranker)
+        self._ranker.reset(self, archive, self._rng)
+
+        # Initialize gradient optimizer
+        self._grad_opt = None
+        if grad_opt == "adam":
+            self._grad_opt = AdamOpt(self._x0, step_size)
+        elif grad_opt == "gradient_ascent":
+            self._grad_opt = GradientAscentOpt(self._x0, step_size)
+        else:
+            raise ValueError(f"Invalid Gradient Ascent Optimizer {grad_opt}")
+
+        if selection_rule not in ["mu", "filter"]:
+            raise ValueError(f"Invalid selection_rule {selection_rule}")
+        self._selection_rule = selection_rule
+
+        if restart_rule not in ["basic", "no_improvement"]:
+            raise ValueError(f"Invalid restart_rule {restart_rule}")
+        self._restart_rule = restart_rule
+
+        # We have a coefficient for each measure and an extra coefficient for
+        # the objective.
+        self._num_coefficients = archive.behavior_dim + 1
+
+        opt_seed = None if seed is None else self._rng.integers(10_000)
+        self.opt = CMAEvolutionStrategy(sigma0, batch_size,
+                                        self._num_coefficients, "truncation",
+                                        opt_seed, self.archive.dtype)
+        self.opt.reset(np.zeros(self._num_coefficients))
+
+        self._batch_size = self.opt.batch_size
+        self._restarts = 0
+
+    @property
+    def x0(self):
+        """numpy.ndarray: Initial solution for the optimizer."""
+        return self._x0
+
+    @property
+    def batch_size(self):
+        """int: Number of solutions to return in :meth:`ask`."""
+        return self._batch_size
+
+    @property
+    def batch_size_dqd(self):
+        """int: Number of solutions to return in :meth:`ask_dqd`.
+
+        This is always 1, as we only return the solution point in
+        :meth:`ask_dqd`.
+        """
+        return 1
+
+    @property
+    def restarts(self):
+        """int: The number of restarts for this emitter."""
+        return self._restarts
+
+    @property
+    def epsilon(self):
+        """int: The epsilon added for numerical stability when normalizing
+        gradients in :meth:`tell_dqd`."""
+        return self._epsilon
+
+    def ask_dqd(self):
+        """Samples a new solution from the gradient optimizer.
+
+        **Call :meth:`ask_dqd` and :meth:`tell_dqd` (in this order) before
+        calling :meth:`ask` and :meth:`tell`.**
+
+        Returns:
+            a new solution to evaluate.
+        """
+        return self._grad_opt.theta[None]
+
+    def ask(self):
+        """Samples new solutions from a gradient aborescence parameterized by a
+        multivariate Gaussian distribution.
+
+        The multivariate Gaussian is parameterized by the evolution strategy
+        optimizer ``self.opt``.
+
+        Note that this method returns `batch_size - 1` solution as one solution
+        is returned via ask_dqd.
+
+        Returns:
+            (batch_size, :attr:`solution_dim`) array -- a batch of new solutions
+            to evaluate.
+        """
+        lower_bounds = np.full(self._num_coefficients,
+                               -np.inf,
+                               dtype=self._archive.dtype)
+        upper_bounds = np.full(self._num_coefficients,
+                               np.inf,
+                               dtype=self._archive.dtype)
+        self._grad_coefficients = self.opt.ask(lower_bounds, upper_bounds)
+        noise = np.expand_dims(self._grad_coefficients, axis=2)
+
+        return self._grad_opt.theta + np.sum(
+            np.multiply(self._jacobian_batch, noise), axis=1)
+
+    def _check_restart(self, num_parents):
+        """Emitter-side checks for restarting the optimizer.
+
+        The optimizer also has its own checks.
+        """
+        if self._restart_rule == "no_improvement":
+            return num_parents == 0
+        return False
+
+    def tell_dqd(self,
+                 solution_batch,
+                 objective_batch,
+                 measures_batch,
+                 jacobian_batch,
+                 status_batch,
+                 value_batch,
+                 metadata_batch=None):
+        """Gives the emitter results from evaluating the gradient of the
+        solutions.
+
+        Args:
+            solution_batch (numpy.ndarray): (batch_size, :attr:`solution_dim`)
+                array of solutions generated by this emitter's :meth:`ask()`
+                method.
+            objective_batch (numpy.ndarray): 1d array containing the objective
+                function value of each solution.
+            measures_batch (numpy.ndarray): (batch_size, measure space
+                dimension) array with the measure space coordinates of each
+                solution.
+            jacobian_batch (numpy.ndarray): ``(batch_size, 1 + measure_dim,
+                solution_dim)`` array consisting of Jacobian matrices of the
+                solutions obtained from :meth:`ask_dqd`. Each matrix should
+                consist of the objective gradient of the solution followed by
+                the measure gradients.
+            status_batch (numpy.ndarray): 1d array of
+                :class:`ribs.archive.addstatus` returned by a series of calls to
+                archive's :meth:`add()` method.
+            value_batch (numpy.ndarray): 1d array of floats returned by a series
+                of calls to archive's :meth:`add()` method. for what these
+                floats represent, refer to :meth:`ribs.archives.add()`.
+            metadata_batch (numpy.ndarray): 1d object array containing a
+                metadata object for each solution.
+        """
+        if self._normalize_grads:
+            norms = (np.linalg.norm(jacobian_batch, axis=2, keepdims=True) +
+                     self._epsilon)
+            jacobian_batch /= norms
+        self._jacobian_batch = jacobian_batch
+
+    def tell(self,
+             solution_batch,
+             objective_batch,
+             measures_batch,
+             status_batch,
+             value_batch,
+             metadata_batch=None):
+        """Gives the emitter results from evaluating solutions.
+
+        The solutions are ranked based on the `rank()` function defined by
+        `self._ranker`.
+
+        Args:
+            solution_batch (numpy.ndarray): (batch_size, :attr:`solution_dim`)
+                array of solutions generated by this emitter's :meth:`ask()`
+                method.
+            objective_batch (numpy.ndarray): 1d array containing the objective
+                function value of each solution.
+            measures_batch (numpy.ndarray): (batch_size, measure space
+                dimension) array with the measure space coordinates of each
+                solution.
+            status_batch (numpy.ndarray): 1d array of
+                :class:`ribs.archive.addstatus` returned by a series of calls to
+                archive's :meth:`add()` method.
+            value_batch (numpy.ndarray): 1d array of floats returned by a series
+                of calls to archive's :meth:`add()` method. for what these
+                floats represent, refer to :meth:`ribs.archives.add()`.
+            metadata_batch (numpy.ndarray): 1d object array containing a
+                metadata object for each solution.
+        """
+        if self._jacobian_batch is None:
+            raise RuntimeError("tell() was called without calling tell_dqd().")
+
+        metadata_batch = itertools.repeat(
+            None) if metadata_batch is None else metadata_batch
+
+        # Count number of new solutions.
+        new_sols = status_batch.astype(bool).sum()
+
+        # Sort the solutions using ranker.
+        indices, ranking_values = self._ranker.rank(
+            self, self.archive, self._rng, solution_batch, objective_batch,
+            measures_batch, status_batch, value_batch, metadata_batch)
+
+        # Select the number of parents.
+        num_parents = (new_sols if self._selection_rule == "filter" else
+                       self._batch_size // 2)
+
+        # Update Evolution Strategy.
+        self.opt.tell(self._grad_coefficients[indices], num_parents)
+
+        # Calculate a new mean in solution space. These weights are from CMA-ES.
+        parents = solution_batch[indices]
+        parents = parents[:num_parents]
+        weights = (np.log(num_parents + 0.5) -
+                   np.log(np.arange(1, num_parents + 1)))
+        weights = weights / np.sum(weights)  # Normalize weights
+        new_mean = np.sum(parents * np.expand_dims(weights, axis=1), axis=0)
+
+        # Use the mean to calculate a gradient step and step the optimizer.
+        gradient_step = new_mean - self._grad_opt.theta
+        self._grad_opt.step(gradient_step)
+
+        # Check for reset.
+        if (self.opt.check_stop(ranking_values[indices]) or
+                self._check_restart(new_sols)):
+            new_coeff = self.archive.sample_elites(1).solution_batch[0]
+            self._grad_opt.reset(new_coeff)
+            self.opt.reset(np.zeros(self._num_coefficients))
+            self._ranker.reset(self, self.archive, self._rng)
+            self._restarts += 1

ribs/emitters/opt/__init__.py
@@ -1,6 +1,9 @@
 """Internal subpackage with optimizers for use across emitters."""
 from ribs.emitters.opt._cma_es import CMAEvolutionStrategy
+from ribs.emitters.opt._gradients import AdamOpt, GradientAscentOpt
 
 __all__ = [
     "CMAEvolutionStrategy",
+    "AdamOpt",
+    "GradientAscentOpt",
 ]

commit e48d57e4e4705b87ee2e8de9df8bea312496e6bf

Fix bug in CMA-ES introduced in #240

ribs/emitters/opt/_cma_es.py
@@ -231,7 +231,9 @@ class CMAEvolutionStrategy:
                        np.log(np.arange(1, num_parents + 1)))
             total_weights = np.sum(weights)
             weights = weights / total_weights
-            mueff = total_weights**2 / np.sum(weights**2)
+            # Note: Since `weights` changes on the line above, np.sum(weights)
+            # is NOT the same as total_weights.
+            mueff = np.sum(weights)**2 / np.sum(weights**2)
         elif self.weight_rule == "active":
             weights = None
 