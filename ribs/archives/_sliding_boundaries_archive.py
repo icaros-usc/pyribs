42fad8ed7192bfe2c8f2da3d294dd404e0758f73
Bryon Tjanaka <bryon.tjanaka@gmail.com>
2022-10-20 15:54:24 -0700

    bc -> measure

--- ribs/archives/_sliding_boundaries_archive.py
+++ ribs/archives/_sliding_boundaries_archive.py
@@ -48,7 +48,7 @@ class SolutionBuffer:
         if self.full():
             # Remove item from the deque.
             _, _, measure_deleted, _ = self._queue.popleft()
-            # Remove bc from sorted lists.
+            # Remove measures from sorted lists.
             for i, m in enumerate(measure_deleted):
                 self._measure_lists[i].remove(m)
 
