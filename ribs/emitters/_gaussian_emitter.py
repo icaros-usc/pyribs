29e4b376ef271ff449216ea912dd1ed2969b3e5c
David Lee <dhlee@usc.edu>
2022-10-21 00:13:17 -0700

    Fix requested changes

--- ribs/emitters/_gaussian_emitter.py
+++ ribs/emitters/_gaussian_emitter.py
@@ -1,6 +1,7 @@
 """Provides the GaussianEmitter."""
 import numpy as np
 
+from ribs._utils import check_batch_shape, check_is_1d
 from ribs.emitters._emitter_base import EmitterBase
 
 
@@ -20,12 +21,12 @@ class GaussianEmitter(EmitterBase):
         archive (ribs.archives.ArchiveBase): An archive to use when creating and
             inserting solutions. For instance, this can be
             :class:`ribs.archives.GridArchive`.
+        sigma (float or array-like): Standard deviation of the Gaussian
+            distribution. Note we assume the Gaussian is diagonal, so if this
+            argument is an array, it must be 1D.
         x0 (array-like): Center of the Gaussian distribution from which to
             sample solutions when the archive is empty. Must be 1-dimensional.
-        sigma (float or array-like): Standard deviation of the Gaussian
-            distribution when the archive is not empty. Note we assume
-            the Gaussian is diagonal, so if this argument is an array, it
-            must be 1D.
+            This argument is irrelevant if ``initial_solutions`` is set.
         initial_solutions (array-like): An (n, solution_dim) array of solutions
             to be used when the archive is empty. If this argument is None, then
             solutions will be sampled from a Gaussian distribution centered at
@@ -45,8 +46,8 @@ class GaussianEmitter(EmitterBase):
 
     def __init__(self,
                  archive,
-                 x0,
                  sigma,
+                 x0=None,
                  initial_solutions=None,
                  bounds=None,
                  batch_size=64,
@@ -54,25 +55,21 @@ class GaussianEmitter(EmitterBase):
         self._rng = np.random.default_rng(seed)
         self._batch_size = batch_size
 
-        self._x0 = np.array(x0, dtype=archive.dtype)
-        if self._x0.ndim != 1:
-            raise ValueError(f"x0 has shape {self._x0.shape}, should be"
-                             f"1-dimensional.")
+        self._sigma = np.array(sigma, dtype=archive.dtype)
 
-        if isinstance(sigma, (float, np.floating)):
-            self._sigma = archive.dtype(sigma)
-        else:
-            self._sigma = np.array(sigma, dtype=archive.dtype)
+        if x0 is None and initial_solutions is None:
+            raise ValueError("At least one of x0 or initial_solutions must "
+                             "be set.")
+
+        self._x0 = np.array(x0, dtype=archive.dtype)
+        check_is_1d(self._x0, "x0")
 
         self._initial_solutions = None
         if initial_solutions is not None:
             self._initial_solutions = np.array(initial_solutions,
                                                dtype=archive.dtype)
-            if self._initial_solutions.shape[1] != archive.solution_dim:
-                raise ValueError(
-                    f"initial_solutions have shape"
-                    f"{self._initial_solutions.shape}; 2nd dimension should"
-                    f"be solution_dim {archive.solution_dim}.")
+            check_batch_shape(self._initial_solutions, "initial_solutions",
+                              archive.solution_dim, "solution_dim")
 
         EmitterBase.__init__(
             self,
@@ -114,22 +111,20 @@ class GaussianEmitter(EmitterBase):
             archive is empty, we return ``self._initial_solutions``, which
             might not have ``batch_size`` solutions.
         """
+        if self.archive.empty:
+            if self._initial_solutions is not None:
+                return np.clip(self._initial_solutions, self.lower_bounds,
+                               self.upper_bounds)
+            parents = np.expand_dims(self.x0, axis=0)
+        else:
+            parents = self.archive.sample_elites(
+                self._batch_size).solution_batch
+
         noise = self._rng.normal(
             scale=self._sigma,
             size=(self._batch_size, self.solution_dim),
         ).astype(self.archive.dtype)
 
-        if self.archive.empty:
-            if self._initial_solutions is None:
-                return np.clip(
-                    np.expand_dims(self.x0, axis=0) + noise,
-                    self.lower_bounds,
-                    self.upper_bounds,
-                )
-            return np.clip(self._initial_solutions, self.lower_bounds,
-                           self.upper_bounds)
-
-        parents = self.archive.sample_elites(self._batch_size).solution_batch
         return np.clip(parents + noise, self.lower_bounds, self.upper_bounds)
 
     def tell(self,
--- ribs/emitters/_iso_line_emitter.py
+++ ribs/emitters/_iso_line_emitter.py
@@ -1,6 +1,7 @@
 """Provides the IsoLineEmitter."""
 import numpy as np
 
+from ribs._utils import check_batch_shape, check_is_1d
 from ribs.emitters._emitter_base import EmitterBase
 
 
@@ -24,12 +25,13 @@ class IsoLineEmitter(EmitterBase):
         archive (ribs.archives.ArchiveBase): An archive to use when creating and
             inserting solutions. For instance, this can be
             :class:`ribs.archives.GridArchive`.
-        x0 (array-like): Center of the Gaussian distribution from which to
-            sample solutions when the archive is empty. Must be 1-dimensional.
         iso_sigma (float): Scale factor for the isotropic distribution used to
-            generate solutions when the archive is non-empty.
+            generate solutions.
         line_sigma (float): Scale factor for the line distribution used when
             generating solutions.
+        x0 (array-like): Center of the Gaussian distribution from which to
+            sample solutions when the archive is empty. Must be 1-dimensional.
+            This argument is irrelevant if ``initial_solutions`` is set.
         initial_solutions (array-like): An (n, solution_dim) array of solutions
             to be used when the archive is empty. If this argument is None, then
             solutions will be sampled from a Gaussian distribution centered at
@@ -47,9 +49,9 @@ class IsoLineEmitter(EmitterBase):
 
     def __init__(self,
                  archive,
-                 x0,
                  iso_sigma=0.01,
                  line_sigma=0.2,
+                 x0=None,
                  initial_solutions=None,
                  bounds=None,
                  batch_size=64,
@@ -57,23 +59,22 @@ class IsoLineEmitter(EmitterBase):
         self._rng = np.random.default_rng(seed)
         self._batch_size = batch_size
 
-        self._x0 = np.array(x0, dtype=archive.dtype)
-        if self._x0.ndim != 1:
-            raise ValueError(f"x0 has shape {self._x0.shape}, should be"
-                             f"1-dimensional.")
-
         self._iso_sigma = archive.dtype(iso_sigma)
         self._line_sigma = archive.dtype(line_sigma)
+
+        if x0 is None and initial_solutions is None:
+            raise ValueError("At least one of x0 or initial_solutions must "
+                             "be set.")
+
+        self._x0 = np.array(x0, dtype=archive.dtype)
+        check_is_1d(self._x0, "x0")
 
         self._initial_solutions = None
         if initial_solutions is not None:
             self._initial_solutions = np.array(initial_solutions,
                                                dtype=archive.dtype)
-            if self._initial_solutions.shape[1] != archive.solution_dim:
-                raise ValueError(
-                    f"initial_solutions have shape"
-                    f"{self._initial_solutions.shape}; 2nd dimension should"
-                    f"be solution_dim {archive.solution_dim}.")
+            check_batch_shape(self._initial_solutions, "initial_solutions",
+                              archive.solution_dim, "solution_dim")
 
         EmitterBase.__init__(
             self,
@@ -117,30 +118,28 @@ class IsoLineEmitter(EmitterBase):
             ``(batch_size, solution_dim)`` array -- contains ``batch_size`` new
             solutions to evaluate.
         """
+        if self.archive.empty and self._initial_solutions is not None:
+            return np.clip(self._initial_solutions, self.lower_bounds,
+                           self.upper_bounds)
+
         iso_gaussian = self._rng.normal(
             scale=self._iso_sigma,
             size=(self._batch_size, self.solution_dim),
         ).astype(self.archive.dtype)
 
         if self.archive.empty:
-            if self._initial_solutions is None:
-                return np.clip(
-                    np.expand_dims(self._x0, axis=0) + iso_gaussian,
-                    self.lower_bounds,
-                    self.upper_bounds,
-                )
-            return np.clip(self._initial_solutions, self.lower_bounds,
-                           self.upper_bounds)
-
-        parents = self.archive.sample_elites(self._batch_size).solution_batch
-        directions = (
-            self.archive.sample_elites(self._batch_size).solution_batch -
-            parents)
-        line_gaussian = self._rng.normal(
-            scale=self._line_sigma,
-            size=(self._batch_size, 1),
-        ).astype(self.archive.dtype)
-        solution_batch = parents + iso_gaussian + line_gaussian * directions
+            solution_batch = np.expand_dims(self._x0, axis=0) + iso_gaussian
+        else:
+            parents = self.archive.sample_elites(
+                self._batch_size).solution_batch
+            directions = (
+                self.archive.sample_elites(self._batch_size).solution_batch -
+                parents)
+            line_gaussian = self._rng.normal(
+                scale=self._line_sigma,
+                size=(self._batch_size, 1),
+            ).astype(self.archive.dtype)
+            solution_batch = parents + iso_gaussian + line_gaussian * directions
 
         return np.clip(solution_batch, self.lower_bounds, self.upper_bounds)
 
