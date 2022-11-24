3fcebbfb9a0751eb44e9a6c4b8a081ad35b9c6e1
David Lee <dhlee@usc.edu>
2022-11-24 05:21:34 -0800

    init

--- ribs/emitters/opt/__init__.py
+++ ribs/emitters/opt/__init__.py
@@ -1,9 +1,17 @@
 """Internal subpackage with optimizers for use across emitters."""
 from ribs.emitters.opt._cma_es import CMAEvolutionStrategy
 from ribs.emitters.opt._gradients import AdamOpt, GradientAscentOpt
+from ribs.emitters.opt._lm_ma_es import LMMAEvolutionStrategy
+from ribs.emitters.opt._openai_es import OpenAIEvolutionStrategy
+from ribs.emitters.opt._optimizer_base import OptimizerBase
+from ribs.emitters.opt._sep_cma_es import SeparableCMAEvolutionStrategy
 
 __all__ = [
     "CMAEvolutionStrategy",
+    "LMMAEvolutionStrategy",
+    "OpenAIEvolutionStrategy",
+    "SeparableCMAEvolutionStrategy",
     "AdamOpt",
     "GradientAscentOpt",
+    "OptimizerBase",
 ]
