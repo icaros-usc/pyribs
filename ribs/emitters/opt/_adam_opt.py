5cf573bfab8ae96bf26cb6b95ca51b5467f171ce
Bryon Tjanaka <bryon.tjanaka@gmail.com>
2022-11-24 22:07:25 -0800

    Add L2 regularization to Adam

--- ribs/emitters/opt/_adam_opt.py
+++ ribs/emitters/opt/_adam_opt.py
@@ -22,6 +22,11 @@ class AdamOpt(GradientOptBase):
         beta1 (float): Exponential decay rate for the moment estimates.
         beta2 (float): Another exponential decay rate for the moment estimates.
         epsilon (float): Hyperparameter for numerical stability.
+        l2_coeff (float): Coefficient for L2 regularization. Note this is
+            **not** the same as "weight decay" -- see `this blog post
+            <https://www.fast.ai/posts/2018-07-02-adam-weight-decay.html>_` and
+            `Loshchilov and Hutler 2019 <https://arxiv.org/abs/1711.05101>_` for
+            more info.
     """
 
     def __init__(self,
@@ -29,11 +34,13 @@ class AdamOpt(GradientOptBase):
                  step_size=0.001,
                  beta1=0.9,
                  beta2=0.999,
-                 epsilon=1e-8):
+                 epsilon=1e-8,
+                 l2_coeff=0.0):
         self._epsilon = epsilon
         self._step_size = step_size
         self._beta1 = beta1
         self._beta2 = beta2
+        self._l2_coeff = l2_coeff
 
         self._theta = None
         self._m = None
@@ -54,6 +61,10 @@ class AdamOpt(GradientOptBase):
 
     def step(self, gradient):
         gradient = np.asarray(gradient)
+
+        # L2 regularization (not weight decay).
+        gradient += self._l2_coeff * self._theta
+
         self._t += 1
         a = (self._step_size * np.sqrt(1 - self._beta2**self._t) /
              (1 - self._beta1**self._t))
