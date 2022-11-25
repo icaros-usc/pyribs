512bdc6bc7291249a2fd308924be860dfa1e1e66
Bryon Tjanaka <bryon.tjanaka@gmail.com>
2022-11-24 20:05:34 -0800

    Add GradientOptBase content

--- ribs/emitters/opt/_gradient_opt_base.py
+++ ribs/emitters/opt/_gradient_opt_base.py
@@ -1,5 +1,34 @@
 """Provides GradientOptBase."""
+from abc import ABC, abstractmethod
 
 
-class GradientOptBase:
-    """Base class for gradient-based optimizers."""
+class GradientOptBase(ABC):
+    """Base class for gradient-based optimizers.
+
+    These optimizers maintain a current solution point :math:`\\theta`. The
+    solution point may be obtained with the :attr:`theta` property, and it may
+    be updated by passing a gradient to :meth:`step`. Finally, the point may be
+    reset with :meth:`reset`.
+    """
+
+    @property
+    @abstractmethod
+    def theta(self):
+        """The current solution point."""
+
+    @abstractmethod
+    def reset(self, theta0):
+        """Resets the solution point to a new value.
+
+        Args:
+            theta0 (np.ndarray): The new solution point.
+        """
+
+    @abstractmethod
+    def step(self, gradient):
+        """Updates the solution based on the given gradient.
+
+        Args:
+            gradient (np.ndarray): The gradient to use for updating the
+                solution.
+        """
