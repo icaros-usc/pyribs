28550128c334ef8ddb2374b513d6ca9012781691
David Lee <dhlee@usc.edu>
2022-11-30 12:07:56 -0800

    docstring

--- ribs/emitters/_gradient_arborescence_emitter.py
+++ ribs/emitters/_gradient_arborescence_emitter.py
@@ -61,7 +61,7 @@ class GradientArborescenceEmitter(EmitterBase):
         es (str): The evolution strategy is a :class:`OptimizerBase` object
             that is used to adapt the distribution from which new solution are
             sampled from. This parameter must be the full or abbreviated
-            optimizer name.
+            optimizer name as described in :mod:`ribs.emitters.opt`.
         es_kwargs (dict): Additional arguments to pass to the evolution
             strategy optimizer. See the evolution-strategy-based optimizers in
             :mod:`ribs.emitters.opt` for the arguments allowed by each
