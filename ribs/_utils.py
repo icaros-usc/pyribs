443d47f8f67dd60d716ce8c55182fd96ff354c87
Bryon Tjanaka <bryon.tjanaka@gmail.com>
2023-09-09 00:51:50 -0700

    Fix to handle issue with metadata handling

--- ribs/_utils.py
+++ ribs/_utils.py
@@ -87,92 +87,95 @@ _BATCH_WARNING = (" Note that starting in pyribs 0.5.0, add() and tell() take"
                   " and tell() only took in a single solution.")
 
 
-def validate_batch_args(archive,
-                        *,
-                        solution_batch,
-                        objective_batch,
-                        measures_batch,
-                        status_batch=None,
-                        value_batch=None,
-                        jacobian_batch=None,
-                        metadata_batch=None):
-    """Performs checks for batch arguments.
+def validate_batch_args(archive, solution_batch, **batch_kwargs):
+    """Preprocesses and validates batch arguments.
 
-    The batch size of each argument is validated with respect to solution_batch.
+    The batch size of each argument in batch_kwargs is validated with respect to
+    solution_batch.
 
     The arguments are assumed to come directly from users, so they may not be
-    arrays. Thus, we preprocess by converting each argument into a numpy array
-    before performing checks. The arguments are then returned in the same order
-    as in the function definition.
+    arrays. Thus, we preprocess each argument by converting it into a numpy
+    array. We then perform checks on the array, including seeing if its batch
+    size matches the batch size of solution_batch. The arguments are then
+    returned in the same order that they were passed into the kwargs, with
+    solution_batch coming first.
+
+    Note that we can guarantee the order is the same as when passed in due to
+    PEP 468 (https://peps.python.org/pep-0468/), which guarantees that kwargs
+    will preserve the same order as they are listed.
+
+    See the for loop for the list of supported kwargs.
     """
+    # List of args to return.
     returns = []
 
+    # Process and validate solution_batch.
     solution_batch = np.asarray(solution_batch)
     check_batch_shape(solution_batch, "solution_batch", archive.solution_dim,
                       "solution_dim", _BATCH_WARNING)
-    batch_size = solution_batch.shape[0]
     returns.append(solution_batch)
 
-    objective_batch = np.asarray(objective_batch)
-    check_is_1d(objective_batch, "objective_batch", _BATCH_WARNING)
-    check_solution_batch_dim(objective_batch,
-                             "objective_batch",
-                             batch_size,
-                             is_1d=True,
-                             extra_msg=_BATCH_WARNING)
-    check_finite(objective_batch, "objective_batch")
-    returns.append(objective_batch)
-
-    measures_batch = np.asarray(measures_batch)
-    check_batch_shape(measures_batch, "measures_batch", archive.measure_dim,
-                      "measure_dim", _BATCH_WARNING)
-    check_solution_batch_dim(measures_batch,
-                             "measures_batch",
-                             batch_size,
-                             is_1d=False,
-                             extra_msg=_BATCH_WARNING)
-    check_finite(measures_batch, "measures_batch")
-    returns.append(measures_batch)
-
-    if jacobian_batch is not None:
-        jacobian_batch = np.asarray(jacobian_batch)
-        check_batch_shape_3d(jacobian_batch, "jacobian_batch",
-                             archive.measure_dim + 1, "measure_dim + 1",
-                             archive.solution_dim, "solution_dim")
-        check_finite(jacobian_batch, "jacobian_batch")
-        returns.append(jacobian_batch)
-
-    if status_batch is not None:
-        status_batch = np.asarray(status_batch)
-        check_is_1d(status_batch, "status_batch", _BATCH_WARNING)
-        check_solution_batch_dim(status_batch,
-                                 "status_batch",
-                                 batch_size,
-                                 is_1d=True,
-                                 extra_msg=_BATCH_WARNING)
-        check_finite(status_batch, "status_batch")
-        returns.append(status_batch)
-
-    if value_batch is not None:
-        value_batch = np.asarray(value_batch)
-        check_is_1d(value_batch, "value_batch", _BATCH_WARNING)
-        check_solution_batch_dim(value_batch,
-                                 "value_batch",
-                                 batch_size,
-                                 is_1d=True,
-                                 extra_msg=_BATCH_WARNING)
-        returns.append(value_batch)
-
-    if metadata_batch is not None:
-        metadata_batch = (np.empty(batch_size, dtype=object) if metadata_batch
-                          is None else np.asarray(metadata_batch, dtype=object))
-        check_is_1d(metadata_batch, "metadata_batch", _BATCH_WARNING)
-        check_solution_batch_dim(metadata_batch,
-                                 "metadata_batch",
-                                 batch_size,
-                                 is_1d=True,
-                                 extra_msg=_BATCH_WARNING)
-        returns.append(metadata_batch)
+    # Process and validate the other batch arguments.
+    batch_size = solution_batch.shape[0]
+    for name, arg in batch_kwargs.items():
+        if name == "objective_batch":
+            objective_batch = np.asarray(arg)
+            check_is_1d(objective_batch, "objective_batch", _BATCH_WARNING)
+            check_solution_batch_dim(objective_batch,
+                                     "objective_batch",
+                                     batch_size,
+                                     is_1d=True,
+                                     extra_msg=_BATCH_WARNING)
+            check_finite(objective_batch, "objective_batch")
+            returns.append(objective_batch)
+        elif name == "measures_batch":
+            measures_batch = np.asarray(arg)
+            check_batch_shape(measures_batch, "measures_batch",
+                              archive.measure_dim, "measure_dim",
+                              _BATCH_WARNING)
+            check_solution_batch_dim(measures_batch,
+                                     "measures_batch",
+                                     batch_size,
+                                     is_1d=False,
+                                     extra_msg=_BATCH_WARNING)
+            check_finite(measures_batch, "measures_batch")
+            returns.append(measures_batch)
+        elif name == "jacobian_batch":
+            jacobian_batch = np.asarray(arg)
+            check_batch_shape_3d(jacobian_batch, "jacobian_batch",
+                                 archive.measure_dim + 1, "measure_dim + 1",
+                                 archive.solution_dim, "solution_dim")
+            check_finite(jacobian_batch, "jacobian_batch")
+            returns.append(jacobian_batch)
+        elif name == "status_batch":
+            status_batch = np.asarray(arg)
+            check_is_1d(status_batch, "status_batch", _BATCH_WARNING)
+            check_solution_batch_dim(status_batch,
+                                     "status_batch",
+                                     batch_size,
+                                     is_1d=True,
+                                     extra_msg=_BATCH_WARNING)
+            check_finite(status_batch, "status_batch")
+            returns.append(status_batch)
+        elif name == "value_batch":
+            value_batch = np.asarray(arg)
+            check_is_1d(value_batch, "value_batch", _BATCH_WARNING)
+            check_solution_batch_dim(value_batch,
+                                     "value_batch",
+                                     batch_size,
+                                     is_1d=True,
+                                     extra_msg=_BATCH_WARNING)
+            returns.append(value_batch)
+        elif name == "metadata_batch":
+            metadata_batch = (np.empty(batch_size, dtype=object)
+                              if arg is None else np.asarray(arg, dtype=object))
+            check_is_1d(metadata_batch, "metadata_batch", _BATCH_WARNING)
+            check_solution_batch_dim(metadata_batch,
+                                     "metadata_batch",
+                                     batch_size,
+                                     is_1d=True,
+                                     extra_msg=_BATCH_WARNING)
+            returns.append(metadata_batch)
 
     return returns
 
