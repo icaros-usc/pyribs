80772bf2c6f19279d759be4231230346d309ca2d
Bryon Tjanaka <bryon.tjanaka@gmail.com>
2023-09-10 07:20:15 -0700

    Comment out tests for now

--- tests/examples.sh
+++ tests/examples.sh
@@ -30,27 +30,27 @@ export OPENBLAS_NUM_THREADS=1
 export OMP_NUM_THREADS=1
 
 # sphere.py
-install_deps examples/sphere.py
-SPHERE_OUTPUT="${TMPDIR}/sphere_output"
-python examples/sphere.py map_elites --itrs 10 --outdir "${SPHERE_OUTPUT}"
-python examples/sphere.py line_map_elites --itrs 10 --outdir "${SPHERE_OUTPUT}"
+# install_deps examples/sphere.py
+# SPHERE_OUTPUT="${TMPDIR}/sphere_output"
+# python examples/sphere.py map_elites --itrs 10 --outdir "${SPHERE_OUTPUT}"
+# python examples/sphere.py line_map_elites --itrs 10 --outdir "${SPHERE_OUTPUT}"
 
-# CVT excluded since it takes a while to build the archive.
-# python examples/sphere.py cvt_map_elites 10 "${SPHERE_OUTPUT}"
-# python examples/sphere.py line_cvt_map_elites 10 "${SPHERE_OUTPUT}"
+# # CVT excluded since it takes a while to build the archive.
+# # python examples/sphere.py cvt_map_elites 10 "${SPHERE_OUTPUT}"
+# # python examples/sphere.py line_cvt_map_elites 10 "${SPHERE_OUTPUT}"
 
-python examples/sphere.py cma_me_imp --itrs 10 --outdir "${SPHERE_OUTPUT}"
-python examples/sphere.py cma_me_imp_mu --itrs 10 --outdir "${SPHERE_OUTPUT}"
-python examples/sphere.py cma_me_rd --itrs 10 --outdir "${SPHERE_OUTPUT}"
-python examples/sphere.py cma_me_rd_mu --itrs 10 --outdir "${SPHERE_OUTPUT}"
-python examples/sphere.py cma_me_opt --itrs 10 --outdir "${SPHERE_OUTPUT}"
-python examples/sphere.py cma_me_mixed --itrs 10 --outdir "${SPHERE_OUTPUT}"
+# python examples/sphere.py cma_me_imp --itrs 10 --outdir "${SPHERE_OUTPUT}"
+# python examples/sphere.py cma_me_imp_mu --itrs 10 --outdir "${SPHERE_OUTPUT}"
+# python examples/sphere.py cma_me_rd --itrs 10 --outdir "${SPHERE_OUTPUT}"
+# python examples/sphere.py cma_me_rd_mu --itrs 10 --outdir "${SPHERE_OUTPUT}"
+# python examples/sphere.py cma_me_opt --itrs 10 --outdir "${SPHERE_OUTPUT}"
+# python examples/sphere.py cma_me_mixed --itrs 10 --outdir "${SPHERE_OUTPUT}"
 
-python examples/sphere.py cma_mega --dim 20 --itrs 10 --outdir "${SPHERE_OUTPUT}"
-python examples/sphere.py cma_mega_adam --dim 20 --itrs 10 --outdir "${SPHERE_OUTPUT}"
+# python examples/sphere.py cma_mega --dim 20 --itrs 10 --outdir "${SPHERE_OUTPUT}"
+# python examples/sphere.py cma_mega_adam --dim 20 --itrs 10 --outdir "${SPHERE_OUTPUT}"
 
-python examples/sphere.py cma_mae --dim 20 --itrs 10 --learning_rate 0.01 --outdir "${SPHERE_OUTPUT}"
-python examples/sphere.py cma_maega --dim 20 --itrs 10 --learning_rate 0.01 --outdir "${SPHERE_OUTPUT}"
+# python examples/sphere.py cma_mae --dim 20 --itrs 10 --learning_rate 0.01 --outdir "${SPHERE_OUTPUT}"
+# python examples/sphere.py cma_maega --dim 20 --itrs 10 --learning_rate 0.01 --outdir "${SPHERE_OUTPUT}"
 
 # lunar_lander.py
 install_deps examples/lunar_lander.py
