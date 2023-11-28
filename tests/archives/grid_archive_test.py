a8eb94c227de7911e2e361c1fd7a719443c4c759
Bryon Tjanaka <bryon.tjanaka@gmail.com>
2023-11-28 11:25:14 -0800

    Fix gridarchive tests

--- tests/archives/grid_archive_test.py
+++ tests/archives/grid_archive_test.py
@@ -102,12 +102,12 @@ def test_add_single_to_archive(data, use_list, add_mode):
         measures = list(data.measures)
 
     if add_mode == "single":
-        status, value = data.archive.add_single(solution, objective, measures)
+        add_info = data.archive.add_single(solution, objective, measures)
     else:
-        status, value = data.archive.add([solution], [objective], [measures])
+        add_info = data.archive.add([solution], [objective], [measures])
 
-    assert status == AddStatus.NEW
-    assert np.isclose(value, data.objective)
+    assert add_info["status"] == AddStatus.NEW
+    assert np.isclose(add_info["value"], data.objective)
     assert_archive_elite(data.archive_with_elite, data.solution, data.objective,
                          data.measures, data.grid_indices)
 
@@ -125,12 +125,12 @@ def test_add_single_to_archive_negative_objective(data, use_list, add_mode):
         measures = list(data.measures)
 
     if add_mode == "single":
-        status, value = data.archive.add_single(solution, objective, measures)
+        add_info = data.archive.add_single(solution, objective, measures)
     else:
-        status, value = data.archive.add([solution], [objective], [measures])
+        add_info = data.archive.add([solution], [objective], [measures])
 
-    assert status == AddStatus.NEW
-    assert np.isclose(value, -data.objective)
+    assert add_info["status"] == AddStatus.NEW
+    assert np.isclose(add_info["value"], -data.objective)
     assert_archive_elite(data.archive_with_elite, data.solution, data.objective,
                          data.measures, data.grid_indices)
 
@@ -139,13 +139,13 @@ def test_add_single_with_low_measures(data, add_mode):
     measures = np.array([-2, -3])
     indices = (0, 0)
     if add_mode == "single":
-        status, _ = data.archive.add_single(data.solution, data.objective,
-                                            measures)
+        add_info = data.archive.add_single(data.solution, data.objective,
+                                           measures)
     else:
-        status, _ = data.archive.add([data.solution], [data.objective],
-                                     [measures])
+        add_info = data.archive.add([data.solution], [data.objective],
+                                    [measures])
 
-    assert status
+    assert add_info["status"]
     assert_archive_elite(data.archive, data.solution, data.objective, measures,
                          indices)
 
@@ -154,12 +154,12 @@ def test_add_single_with_high_measures(data, add_mode):
     measures = np.array([2, 3])
     indices = (9, 19)
     if add_mode == "single":
-        status, _ = data.archive.add_single(data.solution, data.objective,
-                                            measures)
+        add_info = data.archive.add_single(data.solution, data.objective,
+                                           measures)
     else:
-        status, _ = data.archive.add([data.solution], [data.objective],
-                                     [measures])
-    assert status
+        add_info = data.archive.add([data.solution], [data.objective],
+                                    [measures])
+    assert add_info["status"]
     assert_archive_elite(data.archive, data.solution, data.objective, measures,
                          indices)
 
@@ -170,15 +170,16 @@ def test_add_single_and_overwrite(data, add_mode):
     high_objective = data.objective + 1.0
 
     if add_mode == "single":
-        status, value = data.archive_with_elite.add_single(
-            arbitrary_sol, high_objective, data.measures)
+        add_info = data.archive_with_elite.add_single(arbitrary_sol,
+                                                      high_objective,
+                                                      data.measures)
     else:
-        status, value = data.archive_with_elite.add([arbitrary_sol],
-                                                    [high_objective],
-                                                    [data.measures])
+        add_info = data.archive_with_elite.add([arbitrary_sol],
+                                               [high_objective],
+                                               [data.measures])
 
-    assert status == AddStatus.IMPROVE_EXISTING
-    assert np.isclose(value, high_objective - data.objective)
+    assert add_info["status"] == AddStatus.IMPROVE_EXISTING
+    assert np.isclose(add_info["value"], high_objective - data.objective)
     assert_archive_elite(data.archive_with_elite, arbitrary_sol, high_objective,
                          data.measures, data.grid_indices)
 
@@ -189,15 +190,15 @@ def test_add_single_without_overwrite(data, add_mode):
     low_objective = data.objective - 1.0
 
     if add_mode == "single":
-        status, value = data.archive_with_elite.add_single(
-            arbitrary_sol, low_objective, data.measures)
+        add_info = data.archive_with_elite.add_single(arbitrary_sol,
+                                                      low_objective,
+                                                      data.measures)
     else:
-        status, value = data.archive_with_elite.add([arbitrary_sol],
-                                                    [low_objective],
-                                                    [data.measures])
+        add_info = data.archive_with_elite.add([arbitrary_sol], [low_objective],
+                                               [data.measures])
 
-    assert status == AddStatus.NOT_ADDED
-    assert np.isclose(value, low_objective - data.objective)
+    assert add_info["status"] == AddStatus.NOT_ADDED
+    assert np.isclose(add_info["value"], low_objective - data.objective)
     assert_archive_elite(data.archive_with_elite, data.solution, data.objective,
                          data.measures, data.grid_indices)
 
@@ -215,35 +216,35 @@ def test_add_single_threshold_update(add_mode):
 
     # Add a new solution to the archive.
     if add_mode == "single":
-        status, value = archive.add_single(solution, 0.0, measures)
+        add_info = archive.add_single(solution, 0.0, measures)
     else:
-        status_batch, value_batch = archive.add([solution], [0.0], [measures])
-        status, value = status_batch[0], value_batch[0]
+        add_info = archive.add([solution], [0.0], [measures])
+        add_info = {name: arr[0] for name, arr in add_info.items()}
 
-    assert status == 2  # NEW
-    assert np.isclose(value, 1.0)  # 0.0 - (-1.0)
+    assert add_info["status"] == 2  # NEW
+    assert np.isclose(add_info["value"], 1.0)  # 0.0 - (-1.0)
 
     # Threshold should now be (1 - 0.1) * -1.0 + 0.1 * 0.0 = -0.9
 
     # These solutions are below the threshold and should not be inserted.
     if add_mode == "single":
-        status, value = archive.add_single(solution, -0.95, measures)
+        add_info = archive.add_single(solution, -0.95, measures)
     else:
-        status_batch, value_batch = archive.add([solution], [-0.95], [measures])
-        status, value = status_batch[0], value_batch[0]
+        add_info = archive.add([solution], [-0.95], [measures])
+        add_info = {name: arr[0] for name, arr in add_info.items()}
 
-    assert status == 0  # NOT_ADDED
-    assert np.isclose(value, -0.05)  # -0.95 - (-0.9)
+    assert add_info["status"] == 0  # NOT_ADDED
+    assert np.isclose(add_info["value"], -0.05)  # -0.95 - (-0.9)
 
     # These solutions are above the threshold and should be inserted.
     if add_mode == "single":
-        status, value = archive.add_single(solution, -0.8, measures)
+        add_info = archive.add_single(solution, -0.8, measures)
     else:
-        status_batch, value_batch = archive.add([solution], [-0.8], [measures])
-        status, value = status_batch[0], value_batch[0]
+        add_info = archive.add([solution], [-0.8], [measures])
+        add_info = {name: arr[0] for name, arr in add_info.items()}
 
-    assert status == 1  # IMPROVE_EXISTING
-    assert np.isclose(value, 0.1)  # -0.8 - (-0.9)
+    assert add_info["status"] == 1  # IMPROVE_EXISTING
+    assert np.isclose(add_info["value"], 0.1)  # -0.8 - (-0.9)
 
 
 def test_add_single_after_clear(data):
@@ -252,19 +253,19 @@ def test_add_single_after_clear(data):
 
     https://github.com/icaros-usc/pyribs/pull/260
     """
-    status, value = data.archive.add_single(data.solution, data.objective,
-                                            data.measures)
+    add_info = data.archive.add_single(data.solution, data.objective,
+                                       data.measures)
 
-    assert status == 2
-    assert value == data.objective
+    assert add_info["status"] == 2
+    assert add_info["value"] == data.objective
 
     data.archive.clear()
 
-    status, value = data.archive.add_single(data.solution, data.objective,
-                                            data.measures)
+    add_info = data.archive.add_single(data.solution, data.objective,
+                                       data.measures)
 
-    assert status == 2
-    assert value == data.objective
+    assert add_info["status"] == 2
+    assert add_info["value"] == data.objective
 
 
 def test_add_single_wrong_shapes(data):
@@ -283,7 +284,7 @@ def test_add_single_wrong_shapes(data):
 
 
 def test_add_batch_all_new(data):
-    status_batch, value_batch = data.archive.add(
+    add_info = data.archive.add(
         # 4 solutions of arbitrary value.
         solution=[[1, 2, 3]] * 4,
         # The first two solutions end up in separate cells, and the next two end
@@ -291,8 +292,8 @@ def test_add_batch_all_new(data):
         objective=[0, 0, 0, 1],
         measures=[[0, 0], [0.25, 0.25], [0.5, 0.5], [0.5, 0.5]],
     )
-    assert (status_batch == 2).all()
-    assert np.isclose(value_batch, [0, 0, 0, 1]).all()
+    assert (add_info["status"] == 2).all()
+    assert np.isclose(add_info["value"], [0, 0, 0, 1]).all()
 
     assert_archive_elites(
         archive=data.archive,
@@ -305,7 +306,7 @@ def test_add_batch_all_new(data):
 
 
 def test_add_batch_none_inserted(data):
-    status_batch, value_batch = data.archive_with_elite.add(
+    add_info = data.archive_with_elite.add(
         solution=[[1, 2, 3]] * 4,
         objective=[data.objective - 1 for _ in range(4)],
         measures=[data.measures for _ in range(4)],
@@ -313,8 +314,8 @@ def test_add_batch_none_inserted(data):
 
     # All solutions were inserted into the same cell as the elite already in the
     # archive and had objective value 1 less.
-    assert (status_batch == 0).all()
-    assert np.isclose(value_batch, -1.0).all()
+    assert (add_info["status"] == 0).all()
+    assert np.isclose(add_info["value"], -1.0).all()
 
     assert_archive_elites(
         archive=data.archive_with_elite,
@@ -327,7 +328,7 @@ def test_add_batch_none_inserted(data):
 
 
 def test_add_batch_with_improvement(data):
-    status_batch, value_batch = data.archive_with_elite.add(
+    add_info = data.archive_with_elite.add(
         solution=[[1, 2, 3]] * 4,
         objective=[data.objective + 1 for _ in range(4)],
         measures=[data.measures for _ in range(4)],
@@ -335,8 +336,8 @@ def test_add_batch_with_improvement(data):
 
     # All solutions were inserted into the same cell as the elite already in the
     # archive and had objective value 1 greater.
-    assert (status_batch == 1).all()
-    assert np.isclose(value_batch, 1.0).all()
+    assert (add_info["status"] == 1).all()
+    assert np.isclose(add_info["value"], 1.0).all()
 
     assert_archive_elites(
         archive=data.archive_with_elite,
@@ -349,7 +350,7 @@ def test_add_batch_with_improvement(data):
 
 
 def test_add_batch_mixed_statuses(data):
-    status_batch, value_batch = data.archive_with_elite.add(
+    add_info = data.archive_with_elite.add(
         solution=[[1, 2, 3]] * 6,
         objective=[
             # Not added.
@@ -374,8 +375,8 @@ def test_add_batch_mixed_statuses(data):
             [0, 0],
         ],
     )
-    assert (status_batch == [0, 0, 1, 1, 2, 2]).all()
-    assert np.isclose(value_batch, [-1, -2, 1, 2, 1, 2]).all()
+    assert (add_info["status"] == [0, 0, 1, 1, 2, 2]).all()
+    assert np.isclose(add_info["value"], [-1, -2, 1, 2, 1, 2]).all()
 
     assert_archive_elites(
         archive=data.archive_with_elite,
@@ -388,7 +389,7 @@ def test_add_batch_mixed_statuses(data):
 
 
 def test_add_batch_first_solution_wins_in_ties(data):
-    status_batch, value_batch = data.archive_with_elite.add(
+    add_info = data.archive_with_elite.add(
         solution=[
             [1, 2, 3],
             [4, 5, 6],
@@ -410,8 +411,8 @@ def test_add_batch_first_solution_wins_in_ties(data):
             [0, 0],
         ],
     )
-    assert (status_batch == [1, 1, 2, 2]).all()
-    assert np.isclose(value_batch, [1, 1, 3, 3]).all()
+    assert (add_info["status"] == [1, 1, 2, 2]).all()
+    assert np.isclose(add_info["value"], [1, 1, 3, 3]).all()
 
     assert_archive_elites(
         archive=data.archive_with_elite,
@@ -433,7 +434,7 @@ def test_add_batch_not_inserted_if_below_threshold_min():
         learning_rate=0.1,
     )
 
-    status_batch, value_batch = archive.add(
+    add_info = archive.add(
         solution=[[1, 2, 3]] * 4,
         objective=[-20.0, -20.0, 10.0, 10.0],
         measures=[[0.0, 0.0]] * 4,
@@ -441,8 +442,8 @@ def test_add_batch_not_inserted_if_below_threshold_min():
 
     # The first two solutions should not have been inserted since they did not
     # cross the threshold_min of -10.0.
-    assert (status_batch == [0, 0, 2, 2]).all()
-    assert np.isclose(value_batch, [-10.0, -10.0, 20.0, 20.0]).all()
+    assert (add_info["status"] == [0, 0, 2, 2]).all()
+    assert np.isclose(add_info["value"], [-10.0, -10.0, 20.0, 20.0]).all()
 
     assert_archive_elites(
         archive=archive,
@@ -468,7 +469,7 @@ def test_add_batch_threshold_update():
 
     # Add new solutions to the archive in two cells determined by measures and
     # measures2.
-    status_batch, value_batch = archive.add(
+    add_info = archive.add(
         [solution, solution, solution, solution, solution, solution],
         # The first three solutions are inserted since they cross
         # threshold_min, but the last solution is not inserted since it does not
@@ -477,9 +478,10 @@ def test_add_batch_threshold_update():
         [measures, measures, measures, measures2, measures2, measures2],
     )
 
-    assert (status_batch == [2, 2, 2, 2, 2, 0]).all()
+    assert (add_info["status"] == [2, 2, 2, 2, 2, 0]).all()
     assert np.isclose(
-        value_batch, [1.0, 2.0, 3.0, 11.0, 101.0, -9.0]).all()  # [...] - (-1.0)
+        add_info["value"],
+        [1.0, 2.0, 3.0, 11.0, 101.0, -9.0]).all()  # [...] - (-1.0)
 
     # Thresholds based on batch update rule should now be
     # (1 - 0.1)**3 * -1.0 + (0.0 + 1.0 + 2.0) / 3 * (1 - (1 - 0.1)**3) = -0.458
@@ -487,15 +489,15 @@ def test_add_batch_threshold_update():
     # (1 - 0.1)**2 * -1.0 + (10.0 + 100.0) / 2 * (1 - (1 - 0.1)**2) = 9.64
 
     # Mix between solutions which are inserted and not inserted.
-    status_batch, value_batch = archive.add(
+    add_info = archive.add(
         [solution, solution, solution, solution],
         [-0.95, -0.457, 9.63, 9.65],
         [measures, measures, measures2, measures2],
     )
 
-    assert (status_batch == [0, 1, 0, 1]).all()
+    assert (add_info["status"] == [0, 1, 0, 1]).all()
     # [-0.95 - (-0.458), -0.458 - (-0.457), 9.63 - 9.64, 9.65 - 9.64]
-    assert np.isclose(value_batch, [-0.492, 0.001, -0.01, 0.01]).all()
+    assert np.isclose(add_info["value"], [-0.492, 0.001, -0.01, 0.01]).all()
 
     # Thresholds should now be
     # (1 - 0.1)**1 * -0.458 + (-0.457) / 1 * (1 - (1 - 0.1)**1) = -0.4579
@@ -503,14 +505,14 @@ def test_add_batch_threshold_update():
     # (1 - 0.1)**1 * 9.64 + (9.65) / 1 * (1 - (1 - 0.1)**1) = 9.641
 
     # Again mix between solutions which are inserted and not inserted.
-    status_batch, value_batch = archive.add(
+    add_info = archive.add(
         [solution, solution, solution, solution],
         [-0.4580, -0.4578, 9.640, 9.642],
         [measures, measures, measures2, measures2],
     )
 
-    assert (status_batch == [0, 1, 0, 1]).all()
-    assert np.isclose(value_batch, [-0.0001, 0.0001, -0.001, 0.001]).all()
+    assert (add_info["status"] == [0, 1, 0, 1]).all()
+    assert np.isclose(add_info["value"], [-0.0001, 0.0001, -0.001, 0.001]).all()
 
 
 def test_add_batch_threshold_update_inf_threshold_min():
@@ -528,29 +530,30 @@ def test_add_batch_threshold_update_inf_threshold_min():
     measures2 = [-0.1, -0.1]
 
     # Add new solutions to the archive.
-    status_batch, value_batch = archive.add(
+    add_info = archive.add(
         [solution, solution, solution, solution, solution, solution],
         [0.0, 1.0, 2.0, -10.0, 10.0, 100.0],
         [measures, measures, measures, measures2, measures2, measures2],
     )
 
     # Value is same as objective since these are new cells.
-    assert (status_batch == [2, 2, 2, 2, 2, 2]).all()
-    assert np.isclose(value_batch, [0.0, 1.0, 2.0, -10.0, 10.0, 100.0]).all()
+    assert (add_info["status"] == [2, 2, 2, 2, 2, 2]).all()
+    assert np.isclose(add_info["value"],
+                      [0.0, 1.0, 2.0, -10.0, 10.0, 100.0]).all()
 
     # Thresholds are updated based on maximum values in each cell, i.e. 2.0 and
     # 100.0.
 
     # Mix between solutions which are inserted and not inserted.
-    status_batch, value_batch = archive.add(
+    add_info = archive.add(
         [solution, solution, solution, solution],
         [1.0, 10.0, 99.0, 101.0],
         [measures, measures, measures2, measures2],
     )
 
-    assert (status_batch == [0, 1, 0, 1]).all()
+    assert (add_info["status"] == [0, 1, 0, 1]).all()
     # [1.0 - 2.0, 10.0 - 2.0, 99.0 - 100.0, 101.0 - 100.0]
-    assert np.isclose(value_batch, [-1.0, 8.0, -1.0, 1.0]).all()
+    assert np.isclose(add_info["value"], [-1.0, 8.0, -1.0, 1.0]).all()
 
 
 def test_add_batch_wrong_shapes(data):
@@ -576,14 +579,14 @@ def test_add_batch_wrong_shapes(data):
 
 def test_add_batch_zero_length(data):
     """Nothing should happen when adding a batch with length 0."""
-    status_batch, value_batch = data.archive.add(
+    add_info = data.archive.add(
         solution=np.ones((0, 3)),
         objective=np.ones((0,)),
         measures=np.ones((0, 2)),
     )
 
-    assert len(status_batch) == 0
-    assert len(value_batch) == 0
+    assert len(add_info["status"]) == 0
+    assert len(add_info["value"]) == 0
     assert len(data.archive) == 0
 
 
