e428ba73fe7044d40d67076e9cd41c96dd6cfbb8
Bryon Tjanaka <bryon.tjanaka@gmail.com>
2025-07-03 22:42:12 -0700

    Add xp namespace utility

--- ribs/_utils.py
+++ ribs/_utils.py
@@ -1,7 +1,9 @@
 """Miscellaneous internal utilities."""
 import numbers
 
+import array_api_compat.numpy as np_compat
 import numpy as np
+from array_api_compat import array_namespace
 
 
 def check_finite(x, name):
@@ -212,3 +214,17 @@ def readonly(arr):
     """Sets an array to be readonly."""
     arr.flags.writeable = False
     return arr
+
+
+def xp_namespace(xp):
+    """Utility for retrieving a namespace compatible with the array API.
+
+    Expects to receive an argument like `torch` or `numpy`.
+
+    Adapted from scipy:
+    https://github.com/scipy/scipy/blob/4d3dcc103612a2edaec7069638b7f8d0d75cab8b/scipy/signal/windows/_windows.py#L44-L50
+
+    For more context, see:
+    https://github.com/data-apis/array-api-compat/issues/342
+    """
+    return np_compat if xp is None else array_namespace(xp.empty(0))
