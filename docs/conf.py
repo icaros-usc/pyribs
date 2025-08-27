889f95c328a3959ade62b2fdf8254de35d726d17
Bryon Tjanaka <bryon.tjanaka@gmail.com>
2025-08-27 00:31:48 -0700

    Comment

--- docs/conf.py
+++ docs/conf.py
@@ -287,9 +287,7 @@ autodoc_type_aliases = {
     "ArrayLike": "numpy.typing.ArrayLike",
     "DTypeLike": "numpy.typing.DTypeLike",
     "ColorType": "matplotlib.typing.ColorType",
-    # We are okay expanding these aliases because they are quite readable.
-    #  "BatchData": "ribs.typing.BatchData",
-    #  "SingleData": "ribs.typing.SingleData",
+    # We are okay expanding most ribs.typing aliases because they are quite readable.
 }
 autosummary_generate = True
 
