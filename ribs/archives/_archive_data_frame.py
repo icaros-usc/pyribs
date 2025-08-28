9d2c2ad4ca355a765a15e9c02f4856307f6d6f62
Bryon Tjanaka <bryon.tjanaka@gmail.com>
2025-08-28 02:57:16 -0700

    Add annotations to ArchiveDataFrame

--- ribs/archives/_archive_data_frame.py
+++ ribs/archives/_archive_data_frame.py
@@ -1,8 +1,15 @@
 """Provides ArchiveDataFrame."""
 
-import re
+from __future__ import annotations
 
+import re
+from collections.abc import Iterable
+from typing import Any
+
+import numpy as np
 import pandas as pd
+
+from ribs.typing import SingleData
 
 # Developer Notes:
 # - The documentation for this class is hacked -- to add new methods, manually modify
@@ -71,14 +78,14 @@ class ArchiveDataFrame(pd.DataFrame):
         ``get_field("objective")[i]``, and ``get_field("solution")[i]``.
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
 
     @property
     def _constructor(self):
         return ArchiveDataFrame
 
-    def iterelites(self):
+    def iterelites(self) -> Iterable[SingleData]:
         """Iterator that outputs every elite in the ArchiveDataFrame as a dict."""
         # Identify fields in the data frame. There are some edge cases here, such as if
         # someone purposely names their field with an underscore and a number at the end
@@ -108,7 +115,7 @@ class ArchiveDataFrame(pd.DataFrame):
 
         return ({name: arr[i] for name, arr in fields.items()} for i in range(n_elites))
 
-    def get_field(self, field):
+    def get_field(self, field: str) -> np.ndarray | None:
         """Array holding the data for the given field.
 
         None if there is no data for the field.
