b92a50c3d9dc715f869ea05303c752316bf890b2
Bryon Tjanaka <bryon.tjanaka@gmail.com>
2025-12-03 17:30:20 -0800

    Stop skipping lunar lander

--- tests/tutorials_list.sh
+++ tests/tutorials_list.sh
@@ -4,10 +4,6 @@ TUTORIALS=($(ls tutorials/*.ipynb tutorials/*/*.ipynb))
 JSON_LIST=""
 for notebook in "${TUTORIALS[@]}"; do
   case "$notebook" in
-    # This is super flaky due to issues with swig and box2d.
-    tutorials/lunar_lander.ipynb)
-      continue
-      ;;
     # Exclude certain notebooks that take too long.
     tutorials/tom_cruise_dqd.ipynb)
       continue
