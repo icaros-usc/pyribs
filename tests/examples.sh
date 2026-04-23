a4cf8b8d39851561b68a69111a53e9dd4f3d0efb
Bryon Tjanaka <38124174+btjanaka@users.noreply.github.com>
2026-04-22 18:44:12 -0700

    Update logging, outputs, and metrics in examples (#694)
    ## Description
    
    <!-- Provide a brief description of the PR's purpose here. -->
    
    For a while, the logging and outputs in the examples have remained
    nearly the same -- outputs are all saved to a fixed directory, and the
    logging is done with print statements. However, this has a few
    limitations:
    
    - It is difficult to manage multiple runs (you have to pass in `outdir`
    yourself so the outputs don't interfere with each other)
    - Outputs from different algorithms would all be saved in the same
    directory (for sphere.py, at least).
    - There is no easy way to save the logs from the runs with the outputs
    
    Inspired by tools like Hydra, I rearrange the outputs of the examples so
    they fall under a single `logs/` directory, with paths like
    `logs/sphere/cma_me_imp_100/2026-04-21_04-51-31_seed-None/`.
    Furthermore, I add loguru to provide a more modern logging experience
    than print statements -- in particular, logs are now easily saved to a
    file within the output directory.
    
    One downside I see here is that it may be harder for folks to onboard to
    the script due to using more libraries, but I think having these
    features is worthwhile and makes the scripts more useful.
    
    Here's an example of what the new outputs look like for one run of
    sphere.py with cma_me_imp:
    
    ```
    logs/sphere/cma_me_imp_100/2026-04-21_05-22-58_seed-None/
    ├── archive_coverage.png
    ├── archive.csv
    ├── heatmap_00000.png
    ├── heatmap_00250.png
    ├── ...
    ├── heatmap_10000.png
    ├── metrics.json
    ├── out.log
    └── qd_score.png
    ```
    
    ## TODO
    
    <!-- Notable points that this PR has either accomplished or will
    accomplish. -->
    
    - [x] Update sphere.py
    - [x] Update lunar_lander.py
    - [x] Update bop_elites.py
    - [x] Update tests/examples.sh
    - [x] Along the way, I also saw that we were only recording metrics
    every `log_freq` iterations. Metrics are super cheap to record in these
    sccripts, so I changed this to record on every iteration.
    
    ## Status
    
    - [x] I have read the guidelines in
    
    [CONTRIBUTING.md](https://github.com/icaros-usc/pyribs/blob/master/CONTRIBUTING.md)
    - [x] I have linted and formatted my code with `ruff` and `pylint`
    - [x] I have tested my code by running `pytest`
    - [x] I have added a description of my change to the changelog in
    `HISTORY.md`
    - [x] This PR is ready to go

--- tests/examples.sh
+++ tests/examples.sh
@@ -10,9 +10,8 @@ set -e  # Exit if any of the commands fail.
 set -x  # Print out commands as they are run.
 
 TMPDIR="./examples_tmp"
-if [ ! -d "${TMPDIR}" ]; then
-  mkdir "${TMPDIR}"
-fi
+rm -rf ${TMPDIR}
+mkdir "${TMPDIR}"
 
 function install_deps() {
   # Loop through all instances of `pip install` in the script and run the
@@ -44,7 +43,7 @@ for algo in CONFIG:
 for algo in "${SPHERE_ALGOS[@]}"; do
   # CVT excluded since it takes a while to build the archive.
   if [[ "$algo" != @(cvt_map_elites|line_cvt_map_elites) ]]; then
-    python examples/sphere.py "$algo" --itrs 10 --outdir "${SPHERE_OUTPUT}"
+    python examples/sphere.py "$algo" --itrs 10 --outdir "${SPHERE_OUTPUT}/${algo}"
   fi
 done
 
