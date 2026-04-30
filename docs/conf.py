900d3e14a7ab2f2aa7f54b21fa3ad3479b141218
Bryon Tjanaka <bryon.tjanaka@gmail.com>
2026-04-29 23:55:38 -0700

    Fix link for torch docs

--- docs/conf.py
+++ docs/conf.py
@@ -313,7 +313,7 @@ intersphinx_mapping = {
     "shapely": ("https://shapely.readthedocs.io/en/stable/", None),
     "cma": ("https://cma-es.github.io/apidocs-pycma/", None),
     "cupy": ("https://docs.cupy.dev/en/stable/", None),
-    "torch": ("https://docs.pytorch.org/docs/stable/", None),
+    "torch": ("https://docs.pytorch.org/docs/main/", None),
 }
 
 # -- Small custom extensions -------------------------------------------------
