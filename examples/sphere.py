96eeb95c44201f01c000f2f31fa45051ae6ceca8
Bryon Tjanaka <bryon.tjanaka@gmail.com>
2026-05-13 00:58:48 -0700

    Add zuko to deps

--- examples/sphere.py
+++ examples/sphere.py
@@ -1,7 +1,7 @@
 """Runs various QD algorithms on the sphere linear projection benchmark.
 
 Install the following dependencies before running this example:
-    pip install ribs[visualize] torch tqdm fire loguru tabulate
+    pip install ribs[visualize] torch "zuko>=1.0.0" tqdm fire loguru tabulate
 
 The sphere function in this example is adapted from Section 4 of Fontaine 2020
 (https://arxiv.org/abs/1912.02400). Namely, each solution value is clipped to the range
