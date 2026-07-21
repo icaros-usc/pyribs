24df4396d99da25d837e1a1ca24c3034b2f9cfaf
Bryon Tjanaka <bryon.tjanaka@gmail.com>
2026-07-21 01:02:36 -0700

    Update impl

--- ribs/visualize/_aggregate_histogram.py
+++ ribs/visualize/_aggregate_histogram.py
@@ -7,13 +7,16 @@ from typing import Literal
 
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.stats
 from matplotlib.axes import Axes
 from pandas import DataFrame
 
 from ribs.archives import ArchiveBase, ArchiveDataFrame
-from ribs.visualize._utils import compute_vmin_vmax
+from ribs.visualize._utils import compute_vmin_vmax, validate_df
 
 
+# TODO: Support more errorbars.
+# TODO: None default for estimator and errorbar?
 def aggregate_histogram(
     archives: Collection[ArchiveBase],
     ax: Axes | None = None,
@@ -21,10 +24,12 @@ def aggregate_histogram(
     bins: int | Sequence[float] | str | None = 100,
     vmin: float | None = None,
     vmax: float | None = None,
-    # estimator: Literal["mean", "median"] = "mean",
-    # TODO: complementary
+    cumulative: bool | Literal[-1] = False,
+    estimator: Literal["mean", "median"] = "mean",
+    errorbar: Literal["se", "sd"] = "sd",
+    show_edges: bool = True,
 ) -> None:
-    """Plots a CDF aggregated over multiple archives.
+    """Plots a histogram or CDF/CCDF aggregated over multiple archives.
 
     .. info::
 
@@ -40,51 +45,92 @@ def aggregate_histogram(
             instance, calling :meth:`ribs.archives.ArchiveBase.data` with
             ``return_type="pandas"`` and modifying the resulting
             :class:`~ribs.archives.ArchiveDataFrame`. Note that, at a minimum, each
-            dataframe must contain a column for "objective".
+            dataframe must contain a column for "objective". The number of dataframes
+            must be the same as the number of archives.
         bins: Bins for the CDF. The default of 100 indicates that the histogram will
             consist of 100 equally-sized bins. See :func:`~matplotlib.pyplot.hist` for
             more info.
         vmin: Minimum objective value to use in the plot. If ``None``, the minimum
-            objective value in the archives is used.
+            objective value across all the archives is used.
         vmax: Maximum objective value to use in the plot. If ``None``, the maximum
-            objective value in the archives is used.
+            objective value across all the archives is used.
+        cumulative: Pass False to plot a regular histogram, True to plot a CDF, and -1
+            to pass a CCDF.
+        estimator: Method for aggregating the histogram or CDF/CCDF across the multiple
+            archives. For example, if "mean" is passed, we count the number of entries
+            in each histogram bin for each archive, and the final plot shows the mean
+            number of entries in each bin.
+        errorbar: Method for computing the errorbar for the histogram or CDF/CCDF. For
+            example, if "sd" is passed, we display an errorbar showing the standard
+            deviation of the number of entries in each histogram bin.
+        show_edges: Whether to show the edges of the histogram or CDF/CCDF.
     """
-    # TODO: Proper data retrieval.
-    objectives = [archive.data("objective") for archive in archives]
+    if dfs is None:
+        objectives = []
+        for archive in archives:
+            try:
+                objectives.append(archive.data("objective"))
+            except NotImplementedError as e:
+                raise AttributeError(
+                    "To use aggregate_histogram, each archive must have the data() method."
+                ) from e
+    else:
+        if len(dfs) != len(archives):
+            raise ValueError(
+                "If passed in, the number of dfs must equal the number of archives."
+            )
+        objectives = []
+        for df in dfs:
+            df = validate_df(df)
+            objectives.append(np.asarray(df["objective"]))
 
     vmin, vmax = compute_vmin_vmax(vmin, vmax, np.concatenate(objectives))
 
     # Initialize axis.
     ax = plt.gca() if ax is None else ax
 
+    # Compute histogram for each archive.
     histograms = []
     for objs in objectives:
         hist, bin_edges = np.histogram(objs, bins, range=(vmin, vmax))
         histograms.append(hist)
     histograms = np.stack(histograms, axis=0)
 
-    print(histograms)
+    # Apply the cumulative parameter if needed.
+    if cumulative > 0:
+        histograms = np.cumsum(histograms, axis=1)
+    elif cumulative < 0:
+        # TODO: Complementary
+        histograms = ...
+    else:
+        # `cumulative` is False or 0, meaning we leave the histogram as is.
+        ...
 
-    # TODO: Complementary
-    cdf = np.cumsum(histograms, axis=1)
-
-    # TODO: Turn into arg.
-    estimator = "mean"
+    # Aggregate values with `estimator`.
     if estimator == "mean":
-        agg_cdf = cdf.mean(axis=0)
+        agg_hist = histograms.mean(axis=0)
     elif estimator == "median":
-        agg_cdf = cdf.median(axis=0)
+        agg_hist = histograms.median(axis=0)
     else:
         raise ValueError(f"Unknown estimator {estimator}")
 
-    # TODO: spread parameter.
-    spread_cdf = cdf.std(axis=0)
+    # Compute errors/spread with `errorbar`.
+    if errorbar == "sd":
+        err_hist = histograms.std(axis=0)
+    elif errorbar == "se":
+        err_hist = scipy.stats.sem(histograms, axis=0)
+    else:
+        raise ValueError(f"Unknown errorbar {errorbar}")
 
-    patch = ax.stairs(values=agg_cdf, edges=bin_edges)
-    ax.stairs(
-        values=agg_cdf + spread_cdf,
+    patch = ax.stairs(
+        values=agg_hist,
         edges=bin_edges,
-        baseline=agg_cdf - spread_cdf,
+        baseline=0 if show_edges else None,
+    )
+    ax.stairs(
+        values=agg_hist + err_hist,
+        edges=bin_edges,
+        baseline=agg_hist - err_hist,
         fill=True,
         alpha=0.2,
         color=patch.get_edgecolor(),
---
+++ tests/visualize/aggregate_histogram_test.py
@@ -0,0 +1,91 @@
+"""Tests for aggregate_histogram.
+
+See README.md for instructions on writing tests.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pytest
+from matplotlib.testing.decorators import image_comparison
+
+from ribs.archives import GridArchive
+from ribs.visualize import aggregate_histogram
+
+# pylint: disable=redefined-outer-name
+
+
+#
+# Fixtures
+#
+
+
+@pytest.fixture
+def simple_archives():
+    """Three archives intended to have three bins of objective values."""
+    rng = np.random.default_rng(42)
+
+    archives = []
+    for obj_count in [
+        # Number of items in [0, 1), [1, 2), and [2, 3).
+        #
+        # Keep in mind that when plotting a CDF, the CDF does a cumulative sum over the
+        # number of items in the bins. Hence, there should be a high std in the count in
+        # [0, 1), then [1, 2) will have lower std (sums are 55, 60, 65), and finally,
+        # [2, 3) will have zero std (all sums are 100 at that point).
+        [20, 35, 45],
+        [30, 30, 40],
+        [40, 25, 35],
+    ]:
+        # Populate the archive with the negative sphere function.
+        archive = GridArchive(solution_dim=2, dims=[100], ranges=[(0, 1)])
+
+        objectives = np.concatenate(
+            (
+                0.0 + rng.uniform(0, 1, size=obj_count[0]),
+                1.0 + rng.uniform(0, 1, size=obj_count[1]),
+                2.0 + rng.uniform(0, 1, size=obj_count[2]),
+            )
+        )
+
+        archive.add(
+            solution=np.zeros((100, 2)),
+            objective=objectives,
+            measures=np.arange(0, 1, 0.01)[:, None],
+        )
+        archives.append(archive)
+
+    return archives
+
+
+#
+# Tests
+#
+
+
+@image_comparison(
+    baseline_images=["basic_histogram"],
+    remove_text=False,
+    extensions=["png"],
+    style="mpl20",
+)
+def test_basic_histogram(simple_archives):
+    plt.figure(figsize=(8, 6))
+    aggregate_histogram(simple_archives, bins=3)
+
+
+@image_comparison(
+    baseline_images=["basic_cdf"],
+    remove_text=False,
+    extensions=["png"],
+    style="mpl20",
+)
+def test_basic_cdf(simple_archives):
+    plt.figure(figsize=(8, 6))
+    aggregate_histogram(simple_archives, bins=3, cumulative=True)
+
+
+# TODO: vmin and vmax
+# TODO: cumulative
+# TODO: estimator
+# TODO: errorbar
+# TODO: show_edges
